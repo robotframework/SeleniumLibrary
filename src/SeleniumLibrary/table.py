0c11f2f32f42f16b276276e9e5e6d43895487ee9
aek <devnull@localhost>
2010-04-29 10:18:30 +0200

    Tests for "Get Table Cell" and "Table Cell Should Contain" Keyword is green.
    Update Issue 27

--- src/SeleniumLibrary/table.py
+++ src/SeleniumLibrary/table.py
@@ -37,3 +37,44 @@ class Table(object):
         message = "ERROR: Footer in table identified by '%s' should have contained text '%s'." % (table_locator, expected_content)
         self._page_should_contain_element(locator, 'element', message)
         return
+
+    def table_row_should_contain(self, table_locator, row, expected_content):
+        """doc.
+        """
+        locator = "css=table#%s tr:nth-child(%s):contains(\"%s\")" % (table_locator, row, expected_content)
+        message = "ERROR: Row #%s in table identified by '%s' should have contained text '%s'." % (row, table_locator, expected_content)
+        self._page_should_contain_element(locator, 'element', message)
+        return
+
+    def table_column_should_contain(self, table_locator, row, expected_content):
+        """doc.
+        """
+        locator = "css=table#%s tr td:nth-child(%s):contains(\"%s\")" % (table_locator, row, expected_content)
+        message = "ERROR: Column #%s in table identified by '%s' should have contained text '%s'." % (row, table_locator, expected_content)
+        try: 
+            self._page_should_contain_element(locator, 'element', message)
+        except AssertionError, err:
+            if 'should have contained text' not in self._get_error_message(err):
+                raise
+            locator = "css=table#%s tr th:nth-child(%s):contains(\"%s\")" % (table_locator, row, expected_content)
+            self._page_should_contain_element(locator, 'element', message)
+        return    
+    
+    def get_table_cell(self, table_name, row, column):
+        """doc.
+        """
+        row=int(row)-1;
+        column=int(column)-1;
+        return self._selenium.get_table("%s.%d.%d" % (table_name, row, column));
+        
+    def table_cell_should_contain(self, table_locator, row, column, expected_content):
+        message = "ERROR: Cell in table '%s' in row #%s and column #%s should have contained text '%s'." % (table_locator, row, column, expected_content)
+        try:
+            content = self.get_table_cell(table_locator, row, column);
+        except Exception, err:
+            self._info(self._get_error_message(err))
+            raise AssertionError(message)
+        self._info("Cell contains %s." % (content))
+        if not expected_content in content:
+            self._info("Expected content not found.")
+            raise AssertionError(message)
