4ae433dac2ba75aa56408b64af2ddf89830983ff
Janne Härkönen <jth@reaktor.fi>
2010-08-12 19:12:02 +0300

    Added assertion

--- src/SeleniumLibrary/flex.py
+++ src/SeleniumLibrary/flex.py
@@ -15,10 +15,14 @@ class Flex(object):
     def input_text_into_flex(self, locator, value):
         self._flex_command('flexType', 'name=%s,text=%s' % (locator, value))
 
+    def text_in_flex_should_be(self, locator, expected):
+        self._flex_command('flexAssertText', 'name=%s,validator=%s' %
+                           (locator, expected.replace(',', '\\,')))
+
     def _flex_command(self, command, options):
+        # TODO: Howto handle commas in option values??
         app = self._flex_apps.current
         if not app:
-            raise RuntimeError('No Flex application selected')
+            raise RuntimeError('No Flex application selected.')
         self._selenium.do_command(command, [app, options])
-
 
