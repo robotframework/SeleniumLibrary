57480c986641e7b1d3449264d1d9bb06d9a756ad
Janne Härkönen <jth@reaktor.fi>
2010-08-12 18:00:44 +0300

    keyword naming board, cleanup

--- src/SeleniumLibrary/flex.py
+++ src/SeleniumLibrary/flex.py
@@ -2,18 +2,23 @@ class Flex(object):
 
     def select_flex_application(self, locator, alias=None):
         self.page_should_contain_element(locator)
-        self.wait_for_flex_ready(locator)
-        self._flex_apps.register(locator, alias)
+        self._wait_for_flex_ready(locator)
+        return self._flex_apps.register(locator, alias)
 
-    def flex_click(self, locator):
-        self._selenium.do_command('flexClick', [self._flex_apps.current,
-                                                'name=' + locator])
-
-    def wait_for_flex_ready(self, locator, timeout=5000):
+    def _wait_for_flex_ready(self, locator, timeout=5000):
         # It seems that selenium timeout is always used so this timeout has no effect.
         self._selenium.do_command("waitForFlexReady", [locator, timeout])
 
-    def flex_type(self, locator, value):
-        self._selenium.do_command("flexType", [self._flex_apps.current,
-                                               'name=%s,text=%s' % (locator, value)])
+    def click_flex_element(self, locator):
+        self._flex_command('flexClick', 'name='+locator)
+
+    def input_text_into_flex(self, locator, value):
+        self._flex_command('flexType', 'name=%s,text=%s' % (locator, value))
+
+    def _flex_command(self, command, options):
+        app = self._flex_apps.current
+        if not app:
+            raise RuntimeError('No Flex application selected')
+        self._selenium.do_command(command, [app, options])
+
 
