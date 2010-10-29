e0e4b5c0a367815ba910e3606f797b085c90dfb6
Robert Spielmann <spielmann.robert@gmail.com>
2010-10-30 01:42:08 +0200

    keyword naming and documentation enhancements

--- src/SeleniumLibrary/mouse.py
+++ src/SeleniumLibrary/mouse.py
@@ -14,55 +14,51 @@
 
 class Mouse(object):
 
-    def mouse_over(self, locator):
-        """
-        Simulates a user hovering a mouse over the specified element.
+    def simulate_mouse_over(self, locator):
+        """Simulates a user hovering a mouse over the element specified by `locator`.
         
-        `locator` is an element locator.
-        See `introduction` for details about locating elements.
+        Key attributes for arbitrary elements are `id` and `name`. See
+        `introduction` for details about locating elements.
         """
         self._info("Simulating Mouse Over on element '%s'" % locator)
         self._selenium.mouse_over(locator);
 
-    def mouse_out(self, locator):
-        """
-        Simulates a user moving the mouse pointer away from the specified element.
+    def simulate_mouse_out(self, locator):
+        """Simulates a user moving the mouse pointer away from the 
+        element specified by `locator`.
         
-        `locator` is an element locator.
-        See `introduction` for details about locating elements.
+        Key attributes for arbitrary elements are `id` and `name`. See
+        `introduction` for details about locating elements.
         """
         self._info("Simulating Mouse Out on element '%s'" % locator)
         self._selenium.mouse_out(locator)
     
-    def mouse_down(self, locator):
-        """
-        Simulates a user pressing the left mouse button (without releasing it yet) on
-        the specified element.
+    def simulate_mouse_down(self, locator):
+        """Simulates a user pressing the left mouse button (without releasing it yet) on
+        the element specified by `locator`.
         
-        `locator` is an element locator.
-        See `introduction` for details about locating elements.
+        Key attributes for arbitrary elements are `id` and `name`. See
+        `introduction` for details about locating elements.
         """
         self._info("Simulating Mouse Down on element '%s'" % locator)
         self._selenium.mouse_down(locator)
     
-    def mouse_up(self, locator):
-        """
-        Simulates the event that occurs when the user releases the mouse button (i.e., stops
-        holding the button down) on the specified element.
+    def simulate_mouse_up(self, locator):
+        """Simulates the event that occurs when the user releases the mouse button (i.e., stops
+        holding the button down) on the element specified by `locator`.
         
-        `locator` is an element locator.
-        See `introduction` for details about locating elements.
+        Key attributes for arbitrary elements are `id` and `name`. See
+        `introduction` for details about locating elements.
         """
         self._info("Simulating Mouse Up on element '%s'" % locator)
         self._selenium.mouse_up(locator)
 
-    def mouse_move(self, locator):
-        """
-        Simulates a user pressing the mouse button (without releasing it yet) on
-        the specified element.
+    def simulate_mouse_move(self, locator):
+        """Simulates a user pressing the mouse button (without releasing it yet) on
+        the element specified by `locator`.
         
-        `locator` is an element locator.
-        See `introduction` for details about locating elements.
+        Key attributes for arbitrary elements are `id` and `name`. See
+        `introduction` for details about locating elements.
         """
         self._info("Simulating Mouse Move on element '%s'" % locator)
         self._selenium.mouse_move(locator)
