9dae7beabf28cbf5a5328ba277a55dd67257e545
Janne Härkönen <jth@reaktor.fi>
2011-01-11 06:11:16 +0200

    flex keywords: use id as primary locator

--- src/SeleniumLibrary/flex.py
+++ src/SeleniumLibrary/flex.py
@@ -27,14 +27,17 @@ class Flex(RunOnFailure):
         self._selenium.do_command("waitForFlexReady", [locator, timeout])
 
     def click_flex_element(self, locator):
-        self._flex_command('flexClick', 'name='+locator)
+        try:
+            self._flex_command('flexClick', 'id='+locator)
+        except Exception:
+            self._flex_command('flexClick', 'name='+locator)
 
     def input_text_into_flex(self, locator, value):
-        self._flex_command('flexType', 'name=%s,text=%s' % (locator, value))
+        self._flex_command('flexType', 'id=%s,text=%s' % (locator, value))
 
     def text_in_flex_should_be(self, locator, expected):
         self._flex_command('flexAssertText',
-                           'name=%s,validator=%s' % (locator, expected))
+                           'id=%s,validator=%s' % (locator, expected))
 
     def _flex_command(self, command, options):
         # TODO: Howto handle commas in option values??
