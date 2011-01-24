774c3fbcf71a382541eb9a72edafa1f774e1ccb8
Janne Härkönen <jth@reaktor.fi>
2011-01-24 15:52:30 +0200

    seleniumdemo: updated package script to include flex files

--- demo/package.py
+++ demo/package.py
@@ -2,25 +2,28 @@
 
 import os
 from time import localtime
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 
 
 FILES = {'': ['rundemo.py', 'README.txt'],
          'login_tests': ['valid_login.txt', 'invalid_login.txt',
-                         'resource.txt'],
-         'demoapp': ['server.py', 'index.html', 'welcome.html', 'error.html',
-                     'demo.css']}
+                         'html_resource.txt', 'flex_resource.txt'],
+         'demoapp': ['server.py'],
+         'demoapp/html': ['index.html', 'welcome.html', 'error.html',
+                          'demo.css'],
+         'demoapp/flex': ['index.html', 'LoginApp.mxml', 'LoginApp.swf',
+                          'FlexPilot.swf']}
 
 name = 'robotframework-seleniumlibrary-demo'
 root = os.path.dirname(__file__)
-zippath = os.path.join(root, '%s-%s.zip' % (name,
-                                            '%d%02d%02d' % localtime()[:3]))
+timestamp = '%d%02d%02d' % localtime()[:3]
+zippath = os.path.join(root, '%s-%s.zip' % (name, timestamp))
 if os.path.exists(zippath):
     os.remove(zippath)
-zipfile = ZipFile(zippath, 'w')
+zipfile = ZipFile(zippath, 'w', ZIP_DEFLATED)
 for dirname in FILES:
     for filename in FILES[dirname]:
-        path = os.path.join(root, dirname, filename)
+        path = os.path.join(root, dirname.replace('/', os.sep), filename)
         print 'Adding:  ', os.path.normpath(path)
         zipfile.write(path, os.path.join(name, path))
 zipfile.close()
