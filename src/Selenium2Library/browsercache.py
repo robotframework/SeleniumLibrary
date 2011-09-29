b28161a0e058d00bc76a435806dfc65cdce1f424
Ryan Tomac <ryan@tomacfamily.com>
2011-09-29 11:37:47 -0700

    Add BrowserCache class to prevent close from being called during close_all if the browser has already independently been closed, WebDriver browsers don't like having close called more than once

---
+++ src/Selenium2Library/browsercache.py
@@ -0,0 +1,22 @@
+from robot.utils import ConnectionCache
+
+class BrowserCache(ConnectionCache):
+
+    def __init__(self):
+        ConnectionCache.__init__(self, no_current_msg='No current browser')
+        self._closed = set()
+    
+    def close(self, closer_method='close'):
+        if self.current:
+            browser = self.current
+            getattr(browser, closer_method)()
+            self.current = self._no_current
+            self.current_index = None
+            self._closed.add(browser)
+
+    def close_all(self, closer_method='close'):
+        for browser in self._connections:
+            if browser not in self._closed:
+                getattr(browser, closer_method)()
+        self.empty_cache()
+        return self.current
---
+++ test/unit/test_browsercache.py
@@ -0,0 +1,42 @@
+import unittest
+import os
+from Selenium2Library.browsercache import BrowserCache
+
+class MyTests(unittest.TestCase):
+
+    def test_no_current_message(self):
+        cache = BrowserCache()
+        with self.assertRaises(RuntimeError) as context:
+            cache.current.anyMember()
+        self.assertEqual(context.exception.message, "No current browser")
+
+    def test_close_only_called_once(self):
+        cache = BrowserCache()
+
+        browser1 = FakeBrowser()
+        browser2 = FakeBrowser()
+        browser3 = FakeBrowser()
+
+        cache.register(browser1)
+        cache.register(browser2)
+        cache.register(browser3)
+
+        self.assertIs(cache.current, browser3) # sanity check
+        cache.close() # called on browser3
+        self.assertEquals(browser3.close_count, 1) # ensure close called
+
+        cache.close_all()
+        self.assertEquals(browser1.close_count, 1)
+        self.assertEquals(browser2.close_count, 1)
+        self.assertEquals(browser3.close_count, 1)
+
+class FakeBrowser:
+
+    def __init__(self):
+        self.close_count = 0
+
+    def close(self):
+        self.close_count = self.close_count + 1
+
+if __name__ == "__main__":
+    unittest.main()
