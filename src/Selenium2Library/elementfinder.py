7485834727f0c5584e4a701e60a1541e2c3fadbc
Ryan Tomac <ryan@tomacfamily.com>
2011-09-30 16:17:23 -0700

    Add ElementFinder and unit tests

---
+++ src/Selenium2Library/elementfinder.py
@@ -0,0 +1,126 @@
+from robot import utils
+from types import *
+
+class ElementFinder(object):
+
+    def __init__(self):
+        self._strategies = {
+            'class': self._find_by_class_name,
+            'css': self._find_by_css_selector,
+            'id': self._find_by_id,
+            'name': self._find_by_name,
+            'tag': self._find_by_tag_name,
+            'xpath': self._find_by_xpath,
+            None: self._find_by_key_attrs
+        }
+
+    def find(self, browser, locator, tag=None):
+        (prefix, criteria) = self._parse_locator(locator)
+        if tag is not None:
+            tag = self._tag_synonyms.get(tag, tag)
+        strategy = self._strategies.get(prefix)
+        if strategy is None:
+            raise ValueError("Locator with prefix '" + prefix + "' is not supported")
+        return strategy(browser, criteria, tag)
+
+    # Strategy routines, private
+
+    def _find_by_key_attrs(self, browser, criteria, tag):
+        key_attrs = self._key_attrs.get(None)
+        if tag is not None:
+            key_attrs = self._key_attrs.get(tag, key_attrs)
+
+        xpath_criteria = self._xpath_criteria_escape(criteria)
+        xpath_tag = tag if tag is not None else '*'
+        xpath_attrs = ["%s=%s" % (attr, xpath_criteria) for attr in key_attrs]
+        xpath_attrs.extend(
+            self._get_attrs_with_url(key_attrs, criteria, browser))
+        xpath = "//%s[%s]" % (xpath_tag, ' or '.join(xpath_attrs))
+
+        return browser.find_elements_by_xpath(xpath)
+
+    def _find_by_class_name(self, browser, criteria, tag):
+        return self._filter_by_tag(
+            browser.find_elements_by_class_name(criteria),
+            tag)
+
+    def _find_by_css_selector(self, browser, criteria, tag):
+        return self._filter_by_tag(
+            browser.find_elements_by_css_selector(criteria),
+            tag)
+
+    def _find_by_id(self, browser, criteria, tag):
+        return self._filter_by_tag(
+            browser.find_elements_by_id(criteria),
+            tag)
+
+    def _find_by_name(self, browser, criteria, tag):
+        return self._filter_by_tag(
+            browser.find_elements_by_name(criteria),
+            tag)
+
+    def _find_by_tag_name(self, browser, criteria, tag):
+        return self._filter_by_tag(
+            browser.find_elements_by_tag_name(criteria),
+            tag)
+
+    def _find_by_xpath(self, browser, criteria, tag):
+        return self._filter_by_tag(
+            browser.find_elements_by_xpath(criteria),
+            tag)
+
+    # Private
+
+    _tag_synonyms = {
+        'link': 'a',
+        'image': 'img',
+        'radio button': 'input'
+    }
+    _key_attrs = {
+        None: ['@id', '@name'],
+        'a': ['@id', '@name', '@href', 'normalize-space(descendant-or-self::text())'],
+        'img': ['@id', '@name', '@src', '@alt'],
+        'input': ['@id', '@name', '@value', '@src'],
+        'button': ['@id', '@name', '@value', 'normalize-space(descendant-or-self::text())']
+    }
+
+    def _filter_by_tag(self, elements, tag):
+        if tag is None: return elements
+        return filter(
+            lambda element: element.tag_name.lower() == tag,
+            elements)
+
+    def _get_attrs_with_url(self, key_attrs, criteria, browser):
+        attrs = []
+        url = None
+        xpath_url = None
+        for attr in ['@src', '@href']:
+            if attr in key_attrs:
+                if url is None or xpath_url is None:
+                    url = self._get_base_url(browser) + "/" + criteria
+                    xpath_url = self._xpath_criteria_escape(url)
+                attrs.append("%s=%s" % (attr, xpath_url))
+        return attrs
+
+    def _get_base_url(self, browser):
+        url = browser.get_current_url()
+        if '/' in url:
+            url = '/'.join(url.split('/')[:-1])
+        return url
+
+    def _parse_locator(self, locator):
+        prefix = None
+        criteria = locator
+        locator_parts = locator.partition('=')        
+        if len(locator_parts[1]) > 0:
+            prefix = locator_parts[0].lower()
+            criteria = locator_parts[2]
+        return (prefix, criteria)
+
+    def _xpath_criteria_escape(self, str):
+        if '"' in str and '\'' in str:
+            parts_wo_apos = str.split('\'')
+            return "concat('%s')" % "', \"'\", '".join(parts_wo_apos)
+        if '\'' in str:
+            return "\"%s\"" % str
+        return "'%s'" % str
---
+++ test/unit/test_elementfinder.py
@@ -0,0 +1,183 @@
+import unittest
+import os
+from Selenium2Library.elementfinder import ElementFinder
+from mockito import *
+
+class ElementFinderTests(unittest.TestCase):
+
+    def test_find_with_invalid_prefix(self):
+        finder = ElementFinder()
+        browser = mock()
+        with self.assertRaises(ValueError) as context:
+            finder.find(browser, "something=test1")
+        self.assertEqual(context.exception.message, "Locator with prefix 'something' is not supported")
+
+    def test_find_with_no_tag(self):
+        finder = ElementFinder()
+        browser = mock()
+        finder.find(browser, "test1")
+        verify(browser).find_elements_by_xpath("//*[@id='test1' or @name='test1']")
+
+    def test_find_with_tag(self):
+        finder = ElementFinder()
+        browser = mock()
+        finder.find(browser, "test1", tag='div')
+        verify(browser).find_elements_by_xpath("//div[@id='test1' or @name='test1']")
+
+    def test_find_with_locator_with_apos(self):
+        finder = ElementFinder()
+        browser = mock()
+        finder.find(browser, "test '1'")
+        verify(browser).find_elements_by_xpath("//*[@id=\"test '1'\" or @name=\"test '1'\"]")
+
+    def test_find_with_locator_with_quote(self):
+        finder = ElementFinder()
+        browser = mock()
+        finder.find(browser, "test \"1\"")
+        verify(browser).find_elements_by_xpath("//*[@id='test \"1\"' or @name='test \"1\"']")
+
+    def test_find_with_locator_with_quote_and_apos(self):
+        finder = ElementFinder()
+        browser = mock()
+        finder.find(browser, "test \"1\" and '2'")
+        verify(browser).find_elements_by_xpath(
+            "//*[@id=concat('test \"1\" and ', \"'\", '2', \"'\", '') or @name=concat('test \"1\" and ', \"'\", '2', \"'\", '')]")
+
+    def test_find_with_a(self):
+        finder = ElementFinder()
+        browser = mock()
+        when(browser).get_current_url().thenReturn("http://localhost/mypage.html")
+        finder.find(browser, "test1", tag='a')
+        verify(browser).find_elements_by_xpath(
+            "//a[@id='test1' or @name='test1' or @href='test1' or normalize-space(descendant-or-self::text())='test1' or @href='http://localhost/test1']")
+
+    def test_find_with_link_synonym(self):
+        finder = ElementFinder()
+        browser = mock()
+        when(browser).get_current_url().thenReturn("http://localhost/mypage.html")
+        finder.find(browser, "test1", tag='link')
+        verify(browser).find_elements_by_xpath(
+            "//a[@id='test1' or @name='test1' or @href='test1' or normalize-space(descendant-or-self::text())='test1' or @href='http://localhost/test1']")
+
+    def test_find_with_img(self):
+        finder = ElementFinder()
+        browser = mock()
+        when(browser).get_current_url().thenReturn("http://localhost/mypage.html")
+        finder.find(browser, "test1", tag='img')
+        verify(browser).find_elements_by_xpath(
+            "//img[@id='test1' or @name='test1' or @src='test1' or @alt='test1' or @src='http://localhost/test1']")
+
+    def test_find_with_image_synonym(self):
+        finder = ElementFinder()
+        browser = mock()
+        when(browser).get_current_url().thenReturn("http://localhost/mypage.html")
+        finder.find(browser, "test1", tag='image')
+        verify(browser).find_elements_by_xpath(
+            "//img[@id='test1' or @name='test1' or @src='test1' or @alt='test1' or @src='http://localhost/test1']")
+
+    def test_find_with_input(self):
+        finder = ElementFinder()
+        browser = mock()
+        when(browser).get_current_url().thenReturn("http://localhost/mypage.html")
+        finder.find(browser, "test1", tag='input')
+        verify(browser).find_elements_by_xpath(
+            "//input[@id='test1' or @name='test1' or @value='test1' or @src='test1' or @src='http://localhost/test1']")
+
+    def test_find_with_radio_button_synonym(self):
+        finder = ElementFinder()
+        browser = mock()
+        when(browser).get_current_url().thenReturn("http://localhost/mypage.html")
+        finder.find(browser, "test1", tag='radio button')
+        verify(browser).find_elements_by_xpath(
+            "//input[@id='test1' or @name='test1' or @value='test1' or @src='test1' or @src='http://localhost/test1']")
+
+    def test_find_with_button(self):
+        finder = ElementFinder()
+        browser = mock()
+        finder.find(browser, "test1", tag='button')
+        verify(browser).find_elements_by_xpath(
+            "//button[@id='test1' or @name='test1' or @value='test1' or normalize-space(descendant-or-self::text())='test1']")
+
+    def test_find_by_class_name(self):
+        finder = ElementFinder()
+        browser = mock()
+
+        elements = self.make_mock_elements('div', 'a', 'body', 'a')
+        when(browser).find_elements_by_class_name("test1").thenReturn(elements)
+
+        result = finder.find(browser, "class=test1")
+        self.assertEqual(result, elements)
+        result = finder.find(browser, "class=test1", tag='a')
+        self.assertEqual(result, [elements[1], elements[3]])
+
+    def test_find_by_css_selector(self):
+        finder = ElementFinder()
+        browser = mock()
+
+        elements = self.make_mock_elements('div', 'a', 'body', 'a')
+        when(browser).find_elements_by_css_selector("#test1").thenReturn(elements)
+
+        result = finder.find(browser, "css=#test1")
+        self.assertEqual(result, elements)
+        result = finder.find(browser, "css=#test1", tag='a')
+        self.assertEqual(result, [elements[1], elements[3]])
+
+    def test_find_by_id(self):
+        finder = ElementFinder()
+        browser = mock()
+
+        elements = self.make_mock_elements('div', 'a', 'body', 'a')
+        when(browser).find_elements_by_id("test1").thenReturn(elements)
+
+        result = finder.find(browser, "id=test1")
+        self.assertEqual(result, elements)
+        result = finder.find(browser, "id=test1", tag='a')
+        self.assertEqual(result, [elements[1], elements[3]])
+
+    def test_find_by_name(self):
+        finder = ElementFinder()
+        browser = mock()
+
+        elements = self.make_mock_elements('div', 'a', 'body', 'a')
+        when(browser).find_elements_by_name("test1").thenReturn(elements)
+
+        result = finder.find(browser, "name=test1")
+        self.assertEqual(result, elements)
+        result = finder.find(browser, "name=test1", tag='a')
+        self.assertEqual(result, [elements[1], elements[3]])
+
+    def test_find_by_tag_name(self):
+        finder = ElementFinder()
+        browser = mock()
+
+        elements = self.make_mock_elements('div', 'a', 'body', 'a')
+        when(browser).find_elements_by_tag_name("div").thenReturn(elements)
+
+        result = finder.find(browser, "tag=div")
+        self.assertEqual(result, elements)
+        result = finder.find(browser, "tag=div", tag='a')
+        self.assertEqual(result, [elements[1], elements[3]])
+
+    def test_find_by_xpath(self):
+        finder = ElementFinder()
+        browser = mock()
+
+        elements = self.make_mock_elements('div', 'a', 'body', 'a')
+        when(browser).find_elements_by_xpath("//*[@test='1']").thenReturn(elements)
+
+        result = finder.find(browser, "xpath=//*[@test='1']")
+        self.assertEqual(result, elements)
+        result = finder.find(browser, "xpath=//*[@test='1']", tag='a')
+        self.assertEqual(result, [elements[1], elements[3]])
+
+
+    def make_mock_elements(self, *tags):
+        elements = []
+        for tag in tags:
+            element = mock()
+            element.tag_name = tag
+            elements.append(element)
+        return elements
+
+if __name__ == "__main__":
+    unittest.main()
