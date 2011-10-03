56b274a802a493798ad1be20601c20f6f9fd1016
Ryan Tomac <ryan@tomacfamily.com>
2011-10-02 18:19:44 -0700

    BrowserCache should call quit method, not close method, so that all windows are closed

--- src/Selenium2Library/browsercache.py
+++ src/Selenium2Library/browsercache.py
@@ -6,17 +6,17 @@ class BrowserCache(ConnectionCache):
         ConnectionCache.__init__(self, no_current_msg='No current browser')
         self._closed = set()
     
-    def close(self, closer_method='close'):
+    def close(self):
         if self.current:
             browser = self.current
-            getattr(browser, closer_method)()
+            browser.quit()
             self.current = self._no_current
             self.current_index = None
             self._closed.add(browser)
 
-    def close_all(self, closer_method='close'):
+    def close_all(self):
         for browser in self._connections:
             if browser not in self._closed:
-                getattr(browser, closer_method)()
+                browser.quit()
         self.empty_cache()
         return self.current
--- test/unit/test_browsercache.py
+++ test/unit/test_browsercache.py
@@ -16,9 +16,9 @@ class BrowserCacheTests(unittest.TestCase):
         browser = mock()
         cache.register(browser)
 
-        verify(browser, times=0).close() # sanity check
+        verify(browser, times=0).quit() # sanity check
         cache.close()
-        verify(browser, times=1).close()
+        verify(browser, times=1).quit()
 
     def test_close_only_called_once(self):
         cache = BrowserCache()
@@ -32,12 +32,12 @@ class BrowserCacheTests(unittest.TestCase):
         cache.register(browser3)
 
         cache.close()
-        verify(browser3, times=1).close()
+        verify(browser3, times=1).quit()
 
         cache.close_all()
-        verify(browser1, times=1).close()
-        verify(browser2, times=1).close()
-        verify(browser3, times=1).close()
+        verify(browser1, times=1).quit()
+        verify(browser2, times=1).quit()
+        verify(browser3, times=1).quit()
 
 if __name__ == "__main__":
     unittest.main()
