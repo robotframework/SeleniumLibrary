959d0cab62ba251b47f379d34f52d2120816ee46
Ryan Tomac <ryan@tomacfamily.com>
2011-10-06 16:08:27 -0700

    Add get_page_source method to web driver (via monkey patches)

--- src/Selenium2Library/webdrivermonkeypatches.py
+++ src/Selenium2Library/webdrivermonkeypatches.py
@@ -19,6 +19,9 @@ class WebDriverMonkeyPatches:
     def get_current_window_handle(self):
         return self.current_window_handle
 
+    def get_page_source(self):
+        return self.page_source
+
     def get_title(self):
         return self.title
 
@@ -35,6 +38,7 @@ class WebDriverMonkeyPatches:
 
     RemoteWebDriver.get_title = get_title
     RemoteWebDriver.get_current_url = get_current_url
+    RemoteWebDriver.get_page_source = get_page_source
     RemoteWebDriver.get_current_window_handle = get_current_window_handle
     RemoteWebDriver.get_window_handles = get_window_handles
     RemoteWebDriver.set_speed = set_speed
