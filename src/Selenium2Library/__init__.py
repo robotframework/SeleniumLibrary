53ec15c88197e5606b94000f56de9ffcf17dce56
Ryan Tomac <ryan@tomacfamily.com>
2011-10-07 15:34:53 -0700

    Fulfill javascript tests

--- src/Selenium2Library/__init__.py
+++ src/Selenium2Library/__init__.py
@@ -13,6 +13,8 @@ from elementfinder import ElementFinder
 from windowmanager import WindowManager
 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.action_chains import ActionChains
 import webdrivermonkeypatches
     
 FIREFOX_PROFILE_DIR = os.path.join(_THIS_DIR, 'firefoxprofile')
@@ -35,6 +37,7 @@ class Selenium2Library(object):
         self._window_manager = WindowManager()
         self._speed_in_secs = float(0)
         self._timeout_in_secs = float(5)
+        self._cancel_on_next_confirmation = False
 
     def open_browser(self, url, browser='firefox', alias=None):
         self._info("Opening browser '%s' to base url '%s'" % (browser, url))
@@ -442,6 +445,66 @@ class Selenium2Library(object):
             raise AssertionError("Page should have contained text '%s' "
                                  "but did not" % text)
         self._info("Current page contains text '%s'." % text)
+
+    def get_alert_message(self):
+        return self._close_alert()
+
+    def alert_should_be_present(self, text=''):
+        alert_text = self.get_alert_message()
+        if text and alert_text != text:
+            raise AssertionError("Alert text should have been '%s' but was '%s'"
+                                  % (text, alert_text))
+
+    def mouse_down_on_image(self, locator):
+        element = self._element_find(locator, True, True, 'image')
+        ActionChains(self._current_browser()).click_and_hold(element).perform()
+
+    def mouse_down_on_link(self, locator):
+        element = self._element_find(locator, True, True, 'link')
+        ActionChains(self._current_browser()).click_and_hold(element).perform()
+
+    def confirm_action(self):
+        text = self._close_alert(not self._cancel_on_next_confirmation)
+        self._cancel_on_next_confirmation = False
+        return text
+
+    def choose_cancel_on_next_confirmation(self):
+        self._cancel_on_next_confirmation = True
+
+    def choose_ok_on_next_confirmation(self):
+        self._cancel_on_next_confirmation = False
+
+    def execute_javascript(self, *code):
+        js = self._get_javascript_to_execute(''.join(code))
+        self._info("Executing JavaScript:\n%s" % js)
+        return self._current_browser().execute_script(js)
+
+    def open_context_menu(self, locator):
+        element = self._element_find(locator, True, True)
+        ActionChains(self._current_browser()).context_click(element).perform()
+
+    def _get_javascript_to_execute(self, code):
+        codepath = code.replace('/', os.sep)
+        if not (os.path.isabs(codepath) and os.path.isfile(codepath)):
+            return code
+        self._html('Reading JavaScript from file <a href="file://%s">%s</a>.'
+                   % (codepath.replace(os.sep, '/'), codepath))
+        codefile = open(codepath)
+        try:
+            return codefile.read().strip()
+        finally:
+            codefile.close()
+
+    def _close_alert(self, confirm=False):
+        alert = None
+        try:
+            alert = self._current_browser().switch_to_alert()
+            text = ' '.join(alert.text.splitlines()) # collapse new lines chars
+            if not confirm: alert.dismiss()
+            else: alert.accept()
+            return text
+        except WebDriverException:
+            raise RuntimeError('There were no alerts')
 
     def _get_value(self, locator, tag=None):
         element = self._element_find(locator, True, False, tag=tag)
