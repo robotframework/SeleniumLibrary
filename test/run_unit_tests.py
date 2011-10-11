46de6a2886e98e25e485982edc029be675777ffa
Ryan Tomac <ryan@tomacfamily.com>
2011-10-10 23:27:10 -0700

    Add support for recursive unit test loading

--- test/run_unit_tests.py
+++ test/run_unit_tests.py
@@ -3,9 +3,7 @@ import os.path
 import sys
 import unittest
 
-
 def run_unit_tests():
-    testfile = re.compile("^test_.*\.py$", re.IGNORECASE)
     basedir = os.path.abspath(os.path.dirname(__file__))
     testdir = os.path.join(basedir, 'unit')
     path_dirs = [
@@ -18,8 +16,8 @@ def run_unit_tests():
         if path not in sys.path:
             sys.path.insert(0, path)
 
-    tests = [ unittest.defaultTestLoader.loadTestsFromModule(load_module(name))
-              for name in os.listdir(testdir) if testfile.match(name) ]
+    tests = [unittest.defaultTestLoader.loadTestsFromModule(test_module) 
+        for test_module in load_test_modules(testdir)]
 
     runner = unittest.TextTestRunner()
     result = runner.run(unittest.TestSuite(tests))
@@ -27,10 +25,23 @@ def run_unit_tests():
     if rc > 255: rc = 255
     return rc
 
+def load_test_modules(testdir):
+    test_modules = []
+    load_test_modules_recursive(testdir, '', test_modules)
+    return test_modules
 
-def load_module(name):
-    return __import__(os.path.splitext(name)[0])
-
+def load_test_modules_recursive(testdir, relative_dir, test_modules):
+    dir_path = os.path.join(testdir, relative_dir)
+    for name in os.listdir(dir_path):
+        relative_path = os.path.join(relative_dir, name)
+        path = os.path.join(testdir, relative_path)
+        if os.path.isfile(path):
+            if re.match("^test_.*\.py$", name, re.IGNORECASE):
+                module_name = os.path.splitext(relative_path)[0].replace(os.sep, '.')
+                test_modules.append(
+                    __import__(module_name, globals(), locals(), ['*'], -1))
+        else:
+            load_test_modules_recursive(testdir, relative_path, test_modules)
 
 if __name__ == '__main__':
     sys.exit(run_unit_tests())
