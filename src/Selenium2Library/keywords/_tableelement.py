4bd4c5eb617d191a7fe34a418691e3816d8c9b7d
Ryan Tomac <ryan@tomacfamily.com>
2011-10-21 14:24:44 -0700

    Documentation for table-related keywords

--- src/Selenium2Library/keywords/_tableelement.py
+++ src/Selenium2Library/keywords/_tableelement.py
@@ -13,6 +13,14 @@ class _TableElementKeywords(KeywordGroup):
     # Public
 
     def get_table_cell(self, table_locator, row, column, loglevel='INFO'):
+        """Returns the content from a table cell.
+
+        Row and column number start from 1. Header and footer rows are
+        included in the count. This means that also cell content from
+        header or footer rows can be obtained with this keyword. To
+        understand how tables are identified, please take a look at
+        the `introduction`.
+        """
         row = int(row)
         row_index = row - 1
         column = int(column)
@@ -32,6 +40,18 @@ class _TableElementKeywords(KeywordGroup):
             % (table_locator, str(row), str(column)))
 
     def table_cell_should_contain(self, table_locator, row, column, expected, loglevel='INFO'):
+        """Verifies that a certain cell in a table contains `expected`.
+
+        Row and column number start from 1. This keyword passes if the
+        specified cell contains the given content. If you want to test
+        that the cell content matches exactly, or that it e.g. starts
+        with some text, use `Get Table Cell` keyword in combination
+        with built-in keywords such as `Should Be Equal` or `Should
+        Start With`.
+
+        To understand how tables are identified, please take a look at
+        the `introduction`.
+        """
         message = ("Cell in table '%s' in row #%s and column #%s "
                    "should have contained text '%s'."
                    % (table_locator, row, column, expected))
@@ -47,6 +67,24 @@ class _TableElementKeywords(KeywordGroup):
             raise AssertionError(message)
 
     def table_column_should_contain(self, table_locator, col, expected, loglevel='INFO'):
+        """Verifies that a specific column contains `expected`.
+
+        The first leftmost column is column number 1. If the table
+        contains cells that span multiple columns, those merged cells
+        count as a single column. For example both tests below work,
+        if in one row columns A and B are merged with colspan="2", and
+        the logical third column contains "C".
+
+        Example:
+        | Table Column Should Contain | tableId | 3 | C |
+        | Table Column Should Contain | tableId | 2 | C |
+
+        To understand how tables are identified, please take a look at
+        the `introduction`.
+
+        See `Page Should Contain Element` for explanation about
+        `loglevel` argument.
+        """
         element = self._table_element_finder.find_by_col(self._current_browser(), table_locator, col, expected)
         if element is None:
             self.log_source(loglevel)
@@ -55,6 +93,15 @@ class _TableElementKeywords(KeywordGroup):
                    % (col, table_locator, expected))
 
     def table_footer_should_contain(self, table_locator, expected, loglevel='INFO'):
+        """Verifies that the table footer contains `expected`.
+
+        With table footer can be described as any <td>-element that is
+        child of a <tfoot>-element.  To understand how tables are
+        identified, please take a look at the `introduction`.
+
+        See `Page Should Contain Element` for explanation about
+        `loglevel` argument.
+        """
         element = self._table_element_finder.find_by_footer(self._current_browser(), table_locator, expected)
         if element is None:
             self.log_source(loglevel)
@@ -62,6 +109,14 @@ class _TableElementKeywords(KeywordGroup):
                    "text '%s'." % (table_locator, expected))
 
     def table_header_should_contain(self, table_locator, expected, loglevel='INFO'):
+        """Verifies that the table header, i.e. any <th>...</th> element, contains `expected`.
+
+        To understand how tables are identified, please take a look at
+        the `introduction`.
+
+        See `Page Should Contain Element` for explanation about
+        `loglevel` argument.
+        """
         element = self._table_element_finder.find_by_header(self._current_browser(), table_locator, expected)
         if element is None:
             self.log_source(loglevel)
@@ -69,6 +124,21 @@ class _TableElementKeywords(KeywordGroup):
                "text '%s'." % (table_locator, expected))
 
     def table_row_should_contain(self, table_locator, row, expected, loglevel='INFO'):
+        """Verifies that a specific table row contains `expected`.
+
+        The uppermost row is row number 1. For tables that are
+        structured with thead, tbody and tfoot, only the tbody section
+        is searched. Please use `Table Header Should Contain` or
+        `Table Footer Should Contain` for tests against the header or
+        footer content.
+
+        If the table contains cells that span multiple rows, a match
+        only occurs for the uppermost row of those merged cells. To
+        understand how tables are identified, please take a look at
+        the `introduction`.
+
+        See `Page Should Contain Element` for explanation about `loglevel` argument.
+        """
         element = self._table_element_finder.find_by_row(self._current_browser(), table_locator, row, expected)
         if element is None:
             self.log_source(loglevel)
@@ -76,6 +146,14 @@ class _TableElementKeywords(KeywordGroup):
                    "text '%s'." % (row, table_locator, expected))
 
     def table_should_contain(self, table_locator, expected, loglevel='INFO'):
+        """Verifies that `expected` can be found somewhere in the table.
+
+        To understand how tables are identified, please take a look at
+        the `introduction`.
+
+        See `Page Should Contain Element` for explanation about
+        `loglevel` argument.
+        """
         element = self._table_element_finder.find_by_content(self._current_browser(), table_locator, expected)
         if element is None:
             self.log_source(loglevel)
