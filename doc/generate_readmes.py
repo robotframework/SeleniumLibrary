af92caad777a1c4a172e1a2a1cbcc545143fa74b
Ryan Tomac <ryan@tomacfamily.com>
2011-10-24 16:39:36 -0700

    Support building readme files with .rest extension

--- doc/generate_readmes.py
+++ doc/generate_readmes.py
@@ -10,9 +10,9 @@ SRC_DIR = os.path.join(ROOT_DIR, "src")
 LIB_DIR = os.path.join(SRC_DIR, "Selenium2Library")
 
 README_FILES = [
-    "README.txt",
-    "INSTALL.txt",
-    "test/README.txt"
+    "README.rest",
+    "INSTALL.rest",
+    "test/README.rest"
 ]
 
 def main():
@@ -22,16 +22,41 @@ def main():
         print "Readme files will not be built into HTML, docutils not installed"
         return
     for readme_relative_path in README_FILES:
-        readme_abs_path = os.path.join(ROOT_DIR, readme_relative_path.replace('/', os.sep))
-        readme_path_parts = os.path.split(readme_abs_path)
-        Builder().process_txt(readme_path_parts[0], readme_path_parts[1])
-        readme_html_path = os.path.splitext(readme_abs_path)[0] + '.html'
-        target_html_name = os.path.splitext(readme_relative_path.replace('/', '-'))[0] + '.html'
-        target_html_path = os.path.join(THIS_DIR, target_html_name)
-        if os.path.exists(target_html_path):
-            os.remove(target_html_path)
-        os.rename(readme_html_path, target_html_path)
-        print "    ::: Saved: %s" % target_html_name
+        (readme_dir, readme_name) = _parse_readme_path(readme_relative_path)
+        readme_txt_name = _make_txt_file(readme_dir, readme_name)
+        Builder().process_txt(readme_dir, readme_txt_name)
+        _cleanup_txt_file(readme_dir, readme_txt_name)
+
+        readme_html_name = os.path.splitext(readme_name)[0] + '.html'
+        readme_html_path = os.path.join(readme_dir, readme_html_name)
+        target_readme_html_name =  os.path.splitext(readme_relative_path.replace('/', '-'))[0] + '.html'
+        target_readme_html_path = os.path.join(THIS_DIR, target_readme_html_name)
+
+        if os.path.exists(target_readme_html_path):
+            os.remove(target_readme_html_path)
+        os.rename(readme_html_path, target_readme_html_path)
+
+        print "    ::: Saved: %s" % target_readme_html_name
+
+def _parse_readme_path(readme_relative_path):
+    readme_abs_path = os.path.join(ROOT_DIR, readme_relative_path.replace('/', os.sep))
+    readme_path_parts = os.path.split(readme_abs_path)
+    readme_dir = readme_path_parts[0]
+    readme_name = readme_path_parts[1]
+    return (readme_dir, readme_name)
+
+def _make_txt_file(readme_dir, readme_name):
+    readme_txt_name  = os.path.splitext(readme_name)[0] + '.txt'
+    _cleanup_txt_file(readme_dir, readme_txt_name)
+    shutil.copyfile(
+        os.path.join(readme_dir, readme_name), 
+        os.path.join(readme_dir, readme_txt_name))
+    return readme_txt_name
+
+def _cleanup_txt_file(readme_dir, readme_txt_name):
+    readme_txt_abs_path = os.path.join(readme_dir, readme_txt_name)
+    if os.path.exists(readme_txt_abs_path):
+        os.remove(readme_txt_abs_path)
 
 
 if __name__ == '__main__':
