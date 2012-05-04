0aedb27ad023b235a9bc3af3afcea1131f654bd3
Jussi Malinen <jussi.ao.malinen@gmail.com>
2012-05-04 09:41:04 +0300

    generate doc with robot 2.7 or later

--- doc/generate.py
+++ doc/generate.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python
-
-import os, shutil
-from libdoc import LibraryDoc, create_html_doc
-from buildhtml import Builder
-
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.join(THIS_DIR, "..")
-SRC_DIR = os.path.join(ROOT_DIR, "src")
-LIB_DIR = os.path.join(SRC_DIR, "Selenium2Library")
-
-def main():
-    outpath = os.path.join(THIS_DIR, 'Selenium2Library.html')
-    lib = LibraryDoc(LIB_DIR)
-    create_html_doc(lib, outpath)
-    print lib.name, lib.version
-    print outpath
-
-
-if __name__ == '__main__':
-    main()
+from os.path import join, dirname
+try:
+    from robot.libdoc import libdoc
+except:
+    print """Robot Framework 2.7 or later required for generating documentation"""
+else:
+    libdoc(join(dirname(__file__),'..','src','Selenium2Library'), join(dirname(__file__),'Selenium2Library.html'))
