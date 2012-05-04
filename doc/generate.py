805e9f7ac99b69b285e8d126f5499574ff0a9e76
Jussi Malinen <jussi.malinen@reaktor.fi>
2012-05-04 15:50:27 +0300

    fix generate.py to provide a main-method for build_dist.py

--- doc/generate.py
+++ doc/generate.py
@@ -3,6 +3,12 @@ from os.path import join, dirname
 try:
     from robot.libdoc import libdoc
 except:
-    print """Robot Framework 2.7 or later required for generating documentation"""
+    def main():
+        print """Robot Framework 2.7 or later required for generating documentation"""
 else:
-    libdoc(join(dirname(__file__),'..','src','Selenium2Library'), join(dirname(__file__),'Selenium2Library.html'))
+    def main():
+        libdoc(join(dirname(__file__),'..','src','Selenium2Library'), join(dirname(__file__),'Selenium2Library.html'))
+
+
+if __name__ == '__main__':
+    main()
