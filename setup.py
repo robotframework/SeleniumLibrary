cf169fcd349802e95046b22470dd8ce9bbdc78c4
Jussi Malinen <jussi.malinen@reaktor.fi>
2012-05-04 15:03:40 +0300

    update dependency to =>2.6.0. 2.5 is not supported because library uses logging api.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(name         = 'robotframework-selenium2library',
       install_requires = [
 							'decorator >= 3.3.2',
 							'selenium >= 2.8.1',
-							'robotframework == 2.6.3',
+							'robotframework >= 2.6.0',
 							'docutils >= 0.8.1'
 						 ],
       py_modules=['ez_setup'],
