c465bde32fdf991b8fecbfe563cc8ffc151c794b
Kevin Ormbrek <kormbrek@gmail.com>
2014-08-28 21:26:45 -0500

    doc updates related to loglevels and logging

--- src/Selenium2Library/keywords/_browsermanagement.py
+++ src/Selenium2Library/keywords/_browsermanagement.py
@@ -378,7 +378,7 @@ class _BrowserManagementKeywords(KeywordGroup):
         """Logs and returns the entire html source of the current page or frame.
 
         The `loglevel` argument defines the used log level. Valid log levels are
-        WARN, INFO (default), DEBUG, and NONE (no logging).
+        WARN, INFO (default), DEBUG, TRACE and NONE (no logging).
         """
         source = self.get_source()
         self._log(source, loglevel.upper())
