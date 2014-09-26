c4d4ddcedfacf0f4887a8ac40d965f00b505dd64
Kevin Ormbrek <kormbrek@gmail.com>
2014-09-25 21:35:14 -0500

    Document partial link strategy

--- src/Selenium2Library/__init__.py
+++ src/Selenium2Library/__init__.py
@@ -55,6 +55,7 @@ class Selenium2Library(
     | xpath      | Click Element `|` xpath=//div[@id='my_element'] | Matches with arbitrary XPath expression |
     | dom        | Click Element `|` dom=document.images[56] | Matches with arbitrary DOM express            |
     | link       | Click Element `|` link=My Link          | Matches anchor elements by their link text      |
+    | partial link | Click Element `|` partial link=y Lin  | Matches anchor elements by their partial link text |
     | css        | Click Element `|` css=div.my_class      | Matches by CSS selector                         |
     | jquery     | Click Element `|` jquery=div.my_class   | Matches by jQuery/sizzle selector                         |
     | sizzle     | Click Element `|` sizzle=div.my_class   | Matches by jQuery/sizzle selector                         |
