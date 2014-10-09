46a13432180312d8973d169df68275a7361d1ffc
frishber <frishber@ncbi.nlm.nih.gov>
2014-10-09 12:46:39 -0400

    Added comments

--- src/Selenium2Library/keywords/keywordgroup.py
+++ src/Selenium2Library/keywords/keywordgroup.py
@@ -8,18 +8,20 @@ if sys.platform == 'cli':
     decorator = None # decorator module doesn't work with IronPython 2.6
 
 def _run_on_failure_decorator(method, *args, **kwargs):
-    self = args[0]
-    in_keyword = self._in_keyword
-    self._in_keyword = True
+self = args[0]
+    in_keyword = self._in_keyword # If False, we are in the outermost keyword (or in `run_keyword`, if it's a dynamic library)
+    self._in_keyword = True # Set a flag on the instance so that as we call keywords inside this call and this gets run again, we know we're at least one level in.
     try:
         return method(*args, **kwargs)
     except Exception, err:
-        if in_keyword and hasattr(self, '_run_on_failure') and not self._has_run_on_failure:
+        if hasattr(self, '_run_on_failure') and not self._has_run_on_failure:
+            # If we're in an inner keyword, track the fact that we've already run on failure once
             self._has_run_on_failure = True
             self._run_on_failure()
         raise
     finally:
         if not in_keyword:
+            # If we are in the outer call, reset the flags.
             self._in_keyword = False
             self._has_run_on_failure = False
 
