b07cee2ce8c0f7726b87eefa1965b6dc37c4ccea
frishber <frishber@ncbi.nlm.nih.gov>
2014-11-04 13:57:01 -0500

    Tests are working now

--- test/resources/testlibs/CustomSeleniumKeywords.py
+++ test/resources/testlibs/CustomSeleniumKeywords.py
@@ -1,7 +1,13 @@
 from Selenium2Library import Selenium2Library
 from robot.utils import asserts
+from robot.libraries.BuiltIn import BuiltIn
 
 class CustomSeleniumKeywords(Selenium2Library):
+
+    def __init__(self, *args, **kwargs):
+        ret = super(CustomSeleniumKeywords, self).__init__(*args, **kwargs)
+        self._cache = BuiltIn().get_library_instance("Selenium2Library")._cache
+
     def custom_selenium_keyword(self):
         self.custom_selenium_keyword_inner()
 
