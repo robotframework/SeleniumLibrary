ffbe4d0a665d15957a06b717b79a144a8c806206
HelioGuilherme66 <helioxentric@gmail.com>
2015-12-30 19:49:29 +0000

    Removed Python 2.6, added Python 3.4 and robotframework==3.0b1. Added script for dependencies according to Python version. For Python 2.7 installs current robotframework.
    (If there is a better solution, please improve this one).

---
+++ .travis_python_dep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ev
+PYTHON_VERSION=`python -c "import sys; print(sys.version[:3])"`
+if [ "${PYTHON_VERSION}" = "2.7" ]; then
+    pip install robotframework
+    pip install selenium
+    pip install decorator
+    pip install docutils
+    pip install future
+    pip install mockito
+elif [ "${PYTHON_VERSION}" = "3.4" ]; then
+    pip install robotframework==3.0b1
+    pip install selenium
+    pip install decorator
+    pip install docutils
+    pip install future
+fi
