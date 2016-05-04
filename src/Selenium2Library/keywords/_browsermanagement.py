28bcaa0aee68216a11a3a16307786c089aaca719
Evert Kasteel <ekasteel@users.noreply.github.com>
2016-05-04 09:57:04 +0200

    change deprecated switch_to_
    change deprecated switch_to_

--- src/Selenium2Library/keywords/_browsermanagement.py
+++ src/Selenium2Library/keywords/_browsermanagement.py
@@ -287,7 +287,7 @@ class _BrowserManagementKeywords(KeywordGroup):
         """
         self._info("Selecting frame '%s'." % locator)
         element = self._element_find(locator, True, True)
-        self._current_browser().switch_to_frame(element)
+        self._current_browser().switch_to.frame(element)
 
     def select_window(self, locator=None):
         """Selects the window matching locator and return previous window handle.
@@ -336,7 +336,7 @@ class _BrowserManagementKeywords(KeywordGroup):
 
     def unselect_frame(self):
         """Sets the top frame as the current frame."""
-        self._current_browser().switch_to_default_content()
+        self._current_browser().switch_to.default_content()
 
     # Public, browser/current page properties
 
