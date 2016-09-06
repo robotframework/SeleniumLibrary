7930bb186f8f98c5777c2eeae67ba8865a85f408
Pekka Klärck <peke@iki.fi>
2016-09-06 23:42:23 +0300

    Remove vendored mockito and list in requirements-dev.txt instead. (#660)
    * Remove vendored mockito and list in requirements-dev.txt instead.
    
    There absolutely no need for us to keep an old copy of mockito when it can be
    easily pip installed. This also removes the need to make our copy Python 3 compatible.
    
    * Install mockito on travis (and don't install docutils)

--- test/env.py
+++ test/env.py
@@ -4,7 +4,6 @@ import sys
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 UNIT_TEST_DIR = os.path.join(ROOT_DIR, "unit")
 ACCEPTANCE_TEST_DIR = os.path.join(ROOT_DIR, "acceptance")
-LIB_DIR = os.path.join(ROOT_DIR, "lib")
 RESOURCES_DIR = os.path.join(ROOT_DIR, "resources")
 TEST_LIBS_DIR = os.path.join(RESOURCES_DIR, "testlibs")
 RESULTS_DIR = os.path.join(ROOT_DIR, "results")
@@ -12,5 +11,4 @@ HTTP_SERVER_FILE = os.path.join(RESOURCES_DIR, 'testserver', 'testserver.py')
 SRC_DIR = os.path.join(ROOT_DIR, "..", "src")
 
 sys.path.insert(0, SRC_DIR)
-sys.path.append(LIB_DIR)
 sys.path.append(UNIT_TEST_DIR)
