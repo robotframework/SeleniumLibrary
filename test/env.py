22c94d648ff201766097b73b5e4c5cfd131494ed
Pekka Klärck <peke@iki.fi>
2016-12-15 08:38:06 +0200

    Clean up acceptance test execution (#710)
    - Use external robotstatuschecker module instead of an old vendored copy.
    - Add docstring to run_tests.py and use it in when showing usage on the cli.
    - Don't read browser information from environment variables, only use the
      value given from the cli.
    - Use robot.rebot_cli for generating logs/reports instead of running rebot
      on the system.
    - Preserve test server logs.
    - General cleanup here and there.

--- test/env.py
+++ test/env.py
@@ -10,8 +10,6 @@ RESULTS_DIR = os.path.join(ROOT_DIR, "results")
 HTTP_SERVER_FILE = os.path.join(RESOURCES_DIR, "testserver", "testserver.py")
 SRC_DIR = os.path.join(ROOT_DIR, "..", "src")
 
-BROWSER = os.environ.get("BROWSER", "firefox")
-
 TRAVIS = os.environ.get("TRAVIS", None)
 TRAVIS_JOB_NUMBER = os.environ.get("TRAVIS_JOB_NUMBER")
 SAUCE_USERNAME = os.environ.get("SAUCE_USERNAME")
