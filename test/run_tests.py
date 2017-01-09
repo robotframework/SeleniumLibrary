6bdbbb0f9b9c7969341b83b6c629716dffe32d35
Tatu Aalto <aalto.tatu@gmail.com>
2017-01-09 22:43:57 +0200

    Added doc string to the module
    Also the argperser is also using the module doc string.

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,10 +1,43 @@
 #!/usr/bin/env python
+"""Script to run Selenium2Library acceptance tests.
+
+Tests are executed using Robot Framework and results verified automatically
+afterwards using `robotstatuschecker` tool. The tool can be installed using
+`pip install robotstatuschecker` and more information about it can be found
+from: https://github.com/robotframework/statuschecker/. Notice that initially
+some tests may fail.
+
+When running test by using browser from Sauce labs, it is required that the
+Sauce Connect is used. The Sauce Connect allows the browser from Sauce Labs
+reach the acceptance test web server. The acceptance test uses tunnel with
+name `localtunnel` and therefore when establishing the Sauce Connect tunnel
+use the following command:
+    `bin/sc -u YOUR_USERNAME -k YOUR_ACCESS_KEY -i localtunnel`
+
+More details and to downlaod Sauce Connect visit:
+https://wiki.saucelabs.com/display/DOCS/High+Availability+Sauce+Connect+Setup
+
+It is possible to pass Robot Framework command line arguments to the test
+execution as a last arguments to the `run_tests.py` command. It is
+recommended to use arguments to select required suite or test for the
+execution when developing new functionality for the library. Example like
+--test, --suite, --include and --exclude.
+
+Examples:
+
+    run_tests.py chrome
+    run_tests.py --interpreter jython firefox --suite javascript
+    run_tests.py chrome --sauceusername your_username --saucekey account_key \
+--suite javascript
+
+"""
 from __future__ import print_function
 
 from contextlib import contextmanager
 import os
 import sys
 import argparse
+import textwrap
 import subprocess
 
 from robot import rebot_cli
@@ -158,51 +191,19 @@ def sauce_credentials(sauce_username, sauce_key):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Script to run Selenium2Library acceptance tests.',
+        description=__doc__.splitlines()[0],
         formatter_class=argparse.RawTextHelpFormatter,
-        epilog=("""\
-Tests are executed using Robot Framework and results verified automatically \
-afterwards using `robotstatuschecker` tool. The tool can be installed using \
-`pip install robotstatuschecker` and more information about it can be found \
-from: \
-https://github.com/robotframework/statuschecker/. Notice that initially some \
-tests fail.
-
-When running test by using browser from Sauce labs, it is required that the \
-Sauce Connect is used. The Sauce Connect allows the browser from Sauce Labs \
-reach the acceptance test web server. The acceptance test uses tunnel with \
-name `localtunnel` and therefore when establishing the Sauce Connect tunnel \
-use the following command:
-    `bin/sc -u YOUR_USERNAME -k YOUR_ACCESS_KEY -i localtunnel`
-
-More details and to downlaod Sauce Connect visit: \
-https://wiki.saucelabs.com/display/DOCS/High+Availability+Sauce+Connect+Setup
-
-It is possible to pass Robot Framework command line arguments to the test \
-execution as a last arguments to the `run_tests.py` command. It is \
-recommended to use arguments to select required suite or test for the \
-execution when developing new functionality for the library. Example like \
---test, --suite, --include and --exclude.
-
-
-Examples:
-
-    run_tests.py chrome
-    run_tests.py --interpreter jython firefox --suite javascript
-    run_tests.py chrome --sauceusername your_username --saucekey account_key \
---suite javascript
-
-""")
+        epilog='\n'.join(__doc__.splitlines()[2:])
     )
     parser.add_argument(
         '--interpreter',
         '-I',
         default='python',
-        help=(
-            'Any Python interpreter supported by the library (e.g. `python`, '
-            '`jython` or `c:\\Python27\\python.exe`). By default set to '
-            '`python`.)'
-        )
+        help=textwrap.dedent("""\
+            Any Python interpreter supported by the library.
+            E.g. `python`, `jython` or `c:\\Python27\\python.exe`.
+            By default set to `python`.
+        """)
     )
     parser.add_argument(
         'browser',
