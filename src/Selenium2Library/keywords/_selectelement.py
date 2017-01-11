a0ca5e9257269a237511e991eb0c5f17b4b8f681
qitao <qitaos@gmail.com>
2017-01-11 21:22:44 +0800

    Revert "modify argument label=True to value=False"
    This reverts commit 74049a4a214a5d8b6b0d7a153e9aa23a3789b58e.

--- src/Selenium2Library/keywords/_selectelement.py
+++ src/Selenium2Library/keywords/_selectelement.py
@@ -8,8 +8,8 @@ class _SelectElementKeywords(KeywordGroup):
 
     # Public
 
-    def get_list_items(self, locator, value=False):
-        """Returns the values or labels in the select list identified by `locator`.
+    def get_list_items(self, locator, label=True):
+        """Returns the labels or values in the select list identified by `locator`.
 
         Select list keywords work on both lists and combo boxes. Key attributes for
         select lists are `id` and `name`. See `introduction` for details about
@@ -17,15 +17,15 @@ class _SelectElementKeywords(KeywordGroup):
         
         Sample:
         | ${labels1} = | Get List Items | xpath=//h1 |
-        | ${labels2} = | Get List Items | xpath=//h1 | value=${False} |
-        | ${values} = | Get List Items | xpath=//h1 | value=${True} |
+        | ${labels2} = | Get List Items | xpath=//h1 | label=${True} |
+        | ${values} = | Get List Items | xpath=//h1 | label=${False} |
         | Should Be Equal | ${labels1} | ${labels2} |
         """
         select, options = self._get_select_list_options(locator)
-        if value:
-            return self._get_values_for_options(options)
-        else:
+        if label:
             return self._get_labels_for_options(options)
+        else:
+            return self._get_values_for_options(options)
 
     def get_selected_list_label(self, locator):
         """Returns the visible label of the selected element from the select list identified by `locator`.
