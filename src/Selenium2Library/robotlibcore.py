adef16ca123dede0853ed76b2d522fa868413b11
Tatu Aalto <aalto.tatu@gmail.com>
2017-05-22 23:29:14 +0300

    Updated robotlibcore (#807)

--- src/Selenium2Library/robotlibcore.py
+++ src/Selenium2Library/robotlibcore.py
@@ -1,3 +1,24 @@
+# Copyright 2017- Robot Framework Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Generic test library core for Robot Framework.
+
+Main usage is easing creating larger test libraries. For more information and
+examples see the project pages at
+https://github.com/robotframework/PythonLibCore
+"""
+
 import inspect
 import sys
 
@@ -15,6 +36,8 @@ except ImportError:  # Support RF < 2.9
 
 
 PY2 = sys.version_info < (3,)
+
+__version__ = '1.0rc1'
 
 
 class HybridCore(object):
