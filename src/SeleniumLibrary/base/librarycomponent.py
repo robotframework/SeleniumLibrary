5e8bad320469bf6e64f346a08fcd498927be8eed
Tatu Aalto <aalto.tatu@gmail.com>
2017-09-04 23:33:26 +0300

    Rebased with master and resolved conflicts.

--- src/SeleniumLibrary/base/librarycomponent.py
+++ src/SeleniumLibrary/base/librarycomponent.py
@@ -17,8 +17,7 @@
 import os
 
 from robot.api import logger
-from robot.libraries.BuiltIn import BuiltIn
-from robot.libraries.BuiltIn import RobotNotRunningError
+from robot.libraries.BuiltIn import BuiltIn, RobotNotRunningError
 
 from .context import ContextAware
 
