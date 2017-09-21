d7914caf1e2a92a342e23ff7e09111e217fb6341
Tatu Aalto <aalto.tatu@gmail.com>
2017-09-21 22:55:31 +0300

    Added test for ElementFinder and  TableElementFinder in public API

---
+++ test/unit/api/test_public_api_objects.py
@@ -0,0 +1,19 @@
+import unittest
+
+from SeleniumLibrary import SeleniumLibrary
+from SeleniumLibrary.locators import ElementFinder
+from SeleniumLibrary.locators import TableElementFinder
+
+
+class TableElementFinderInAPITest(unittest.TestCase):
+
+    def test_table_finder(self):
+        sl = SeleniumLibrary()
+        self.assertIsInstance(sl.table_element_finder, TableElementFinder)
+
+
+class ElementFinderInAPITest(unittest.TestCase):
+
+    def test_table_finder(self):
+        sl = SeleniumLibrary()
+        self.assertIsInstance(sl.element_finder, ElementFinder)
