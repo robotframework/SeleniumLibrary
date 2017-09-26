572a3e91e275b58bac371c5904ec897141cbe4ff
Pekka Klärck <peke@iki.fi>
2017-09-26 16:11:34 +0300

    Set version to .dev1 and regen docs

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -36,7 +36,7 @@ from SeleniumLibrary.utils import (BrowserCache, Deprecated, LibraryListener,
                                    timestr_to_secs)
 
 
-__version__ = '3.0.0b1'
+__version__ = '3.0.0b2.dev1'
 
 
 class SeleniumLibrary(DynamicCore):
