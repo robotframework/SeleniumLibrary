d887c6e6572d751183108a4ea2ab782503117ead
Tatu Aalto <aalto.tatu@gmail.com>
2018-03-05 22:39:19 +0200

    Updated version number to 3.2.0.dev1

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -39,7 +39,7 @@ from SeleniumLibrary.locators import ElementFinder
 from SeleniumLibrary.utils import Deprecated, LibraryListener, timestr_to_secs
 
 
-__version__ = '3.1.1.dev1'
+__version__ = '3.2.0.dev1'
 
 
 class SeleniumLibrary(DynamicCore):
