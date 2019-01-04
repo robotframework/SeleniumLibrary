f04bf517923aea5e6b42b914d743e18acf0838d4
Tatu Aalto <aalto.tatu@gmail.com>
2019-01-04 22:32:35 +0200

    Fixed remote_url and capabilites for htmlunit

--- utest/test/keywords/test_webdrivercreator.py
+++ utest/test/keywords/test_webdrivercreator.py
@@ -359,7 +359,7 @@ class WebDriverCreatorTests(unittest.TestCase):
         driver = self.creator.create_phantomjs({'desired_capabilities': capabilities}, url)
         self.assertEqual(driver, expected_webdriver)
 
-    def test_htmlunit(self):
+    def test_htmlunit_no_caps(self):
         caps = webdriver.DesiredCapabilities.HTMLUNIT
         expected_webdriver = mock()
         when(webdriver).Remote(command_executor='None',
@@ -367,6 +367,16 @@ class WebDriverCreatorTests(unittest.TestCase):
                                browser_profile=None,
                                options=None).thenReturn(expected_webdriver)
         driver = self.creator.create_htmlunit({}, None)
+        self.assertEqual(driver, expected_webdriver)
+
+    def test_htmlunit_no_caps(self):
+        caps = {"browserName": "htmlunit"}
+        expected_webdriver = mock()
+        when(webdriver).Remote(command_executor='None',
+                               desired_capabilities=caps,
+                               browser_profile=None,
+                               options=None).thenReturn(expected_webdriver)
+        driver = self.creator.create_htmlunit({'desired_capabilities': caps}, None)
         self.assertEqual(driver, expected_webdriver)
 
     def test_htmlunit_with_js(self):
