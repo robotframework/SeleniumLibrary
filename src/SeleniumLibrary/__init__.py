66beb23cfbb8d973a334a134563eb227a9654faf
Tatu Aalto <aalto.tatu@gmail.com>
2019-01-04 23:57:35 +0200

    Back to dev version

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -39,7 +39,7 @@ from SeleniumLibrary.locators import ElementFinder
 from SeleniumLibrary.utils import Deprecated, LibraryListener, timestr_to_secs
 
 
-__version__ = '3.3.1'
+__version__ = '3.3.2.dev1'
 
 
 class SeleniumLibrary(DynamicCore):
