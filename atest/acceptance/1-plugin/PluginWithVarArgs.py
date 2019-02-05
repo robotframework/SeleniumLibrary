33272417266377208b7bd9c9051d5a3ea6fb23a2
Tatu Aalto <aalto.tatu@gmail.com>
2019-02-05 20:05:35 +0200

    Fixing test library

--- atest/acceptance/1-plugin/PluginWithVarArgs.py
+++ atest/acceptance/1-plugin/PluginWithVarArgs.py
@@ -3,13 +3,13 @@ from SeleniumLibrary.base import LibraryComponent, keyword
 
 class PluginWithVarArgs(LibraryComponent):
 
-        def __init__(self, ctx, *kwargs):
+        def __init__(self, ctx, *args):
             LibraryComponent.__init__(self, ctx)
-            self.kwargs = kwargs
+            self.args = args
 
         @keyword
         def return_var_args_as_string(self):
             joined_str = 'start:'
-            for arg in self.kwargs:
+            for arg in self.args:
                 joined_str = '%s %s,' % (joined_str, arg)
             return joined_str[:-1]
