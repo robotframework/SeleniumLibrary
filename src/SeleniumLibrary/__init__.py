2c0116d34f0c607b55a89d61a49a4c2de00a1154
Mihai Pârvu <4466614+mihaiparvu@users.noreply.github.com>
2019-03-20 15:03:32 +0200

    Remove deprecated attributes (#1347)
    All deprecated methods and attributes from the library __init__ are removed.

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -16,7 +16,6 @@
 
 from collections import namedtuple
 from inspect import isclass
-import warnings
 
 from robot.api import logger
 from robot.errors import DataError
@@ -40,7 +39,7 @@ from SeleniumLibrary.keywords import (AlertKeywords,
                                       WebDriverCache,
                                       WindowKeywords)
 from SeleniumLibrary.locators import ElementFinder
-from SeleniumLibrary.utils import Deprecated, LibraryListener, timestr_to_secs, is_truthy
+from SeleniumLibrary.utils import LibraryListener, timestr_to_secs, is_truthy
 
 
 __version__ = '4.0.0.dev1'
@@ -556,13 +555,6 @@ class SeleniumLibrary(DynamicCore):
         else:
             self.event_firing_webdriver = None
 
-    _speed_in_secs = Deprecated('_speed_in_secs', 'speed')
-    _timeout_in_secs = Deprecated('_timeout_in_secs', 'timeout')
-    _implicit_wait_in_secs = Deprecated('_implicit_wait_in_secs',
-                                        'implicit_wait')
-    _run_on_failure_keyword = Deprecated('_run_on_failure_keyword',
-                                         'run_on_failure_keyword')
-
     def run_keyword(self, name, args, kwargs):
         try:
             return DynamicCore.run_keyword(self, name, args, kwargs)
@@ -646,24 +638,6 @@ class SeleniumLibrary(DynamicCore):
         """
         return self._element_finder.find(locator, first_only=False,
                                          required=False, parent=parent)
-
-    @property
-    def _cache(self):
-        warnings.warn('"SeleniumLibrary._cache" is deprecated, '
-                      'use public API instead.', DeprecationWarning)
-        return self._drivers
-
-    def _current_browser(self):
-        warnings.warn('"SeleniumLibrary._current_browser" is deprecated, '
-                      'use "SeleniumLibrary.driver" instead.',
-                      DeprecationWarning)
-        return self.driver
-
-    def _run_on_failure(self):
-        warnings.warn('"SeleniumLibrary._run_on_failure" is deprecated, '
-                      'use "SeleniumLibrary.failure_occurred" instead.',
-                      DeprecationWarning)
-        self.failure_occurred()
 
     def _parse_plugins(self, plugins):
         libraries = []
