8775ff33ac4f0bb9c180ce611d7124ce1aed2d42
mparvu <mihaiparvu@users.noreply.github.com>
2019-03-20 14:22:47 +0200

    Remove unused deprecated.py file

--- src/SeleniumLibrary/utils/__init__.py
+++ src/SeleniumLibrary/utils/__init__.py
@@ -16,7 +16,6 @@
 
 from robot.utils import plural_or_not, secs_to_timestr, timestr_to_secs
 
-from .deprecated import Deprecated
 from .librarylistener import LibraryListener
 from .seleniumversion import SELENIUM_VERSION
 from .types import is_falsy, is_noney, is_string, is_truthy, PY3
