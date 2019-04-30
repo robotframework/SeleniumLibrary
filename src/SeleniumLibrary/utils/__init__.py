cf13be17a3d08c2d9df8c1b660441d5759fcab15
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2019-04-30 20:16:25 +0100

    Removed unused seleniumversion (#1382)
    Fixes #1380

--- src/SeleniumLibrary/utils/__init__.py
+++ src/SeleniumLibrary/utils/__init__.py
@@ -17,7 +17,6 @@
 from robot.utils import plural_or_not, secs_to_timestr, timestr_to_secs
 
 from .librarylistener import LibraryListener
-from .seleniumversion import SELENIUM_VERSION
 from .types import is_falsy, is_noney, is_string, is_truthy, PY3
 
 
