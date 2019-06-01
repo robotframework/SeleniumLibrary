c29f2ad27c3e1b65cb3e01ad5868eada7365b790
Tatu Aalto <aalto.tatu@gmail.com>
2019-06-02 01:01:08 +0300

    Selenium options for methods

--- src/SeleniumLibrary/keywords/webdrivertools.py
+++ src/SeleniumLibrary/keywords/webdrivertools.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import importlib
 import inspect
 import os
 import warnings
@@ -338,3 +339,17 @@ class SeleniumOptions(object):
             else:
                 result.append(opt)
         return result
+
+    def create(self, browser, options):
+        selenium_options = self._import_options(browser)
+        selenium_options = selenium_options()
+        for option in options:
+            for key in option:
+                attr = getattr(selenium_options, key)
+                attr(*option[key])
+        return selenium_options
+
+    def _import_options(self, browser):
+        browser = browser.replace('headless_', '', 1)
+        options = importlib.import_module('selenium.webdriver.%s.options' % browser)
+        return options.Options
--- utest/test/keywords/test_selenium_options_parser.py
+++ utest/test/keywords/test_selenium_options_parser.py
@@ -65,9 +65,49 @@ class SeleniumOptionsParserTests(unittest.TestCase):
         self.result_formatter()
         verify_all('Selenium options escape string to dict', self.results, reporter=self.reporter)
 
+    @unittest.skipIf(JYTHON, 'ApprovalTest does not work with Jython')
+    def test_options_create(self):
+        options = [{'add_argument': ['--disable-dev-shm-usage']}]
+        sel_options = self.options.create('chrome', options)
+        self.results.append(sel_options.arguments)
+
+        options.append({'add_argument': ['--headless']})
+        sel_options = self.options.create('chrome', options)
+        self.results.append(sel_options.arguments)
+
+        options.append({'add_argument': ['--proxy-server=66.97.38.58:80']})
+        sel_options = self.options.create('chrome', options)
+        self.results.append(sel_options.arguments)
+
+        self.result_formatter()
+        verify_all('Selenium options', self.results, reporter=self.reporter)
+
+    @unittest.skipIf(JYTHON, 'ApprovalTest does not work with Jython')
+    def test_importer(self):
+        self.results.append(self.options._import_options('firefox'))
+        self.results.append(self.options._import_options('headless_firefox'))
+        self.results.append(self.options._import_options('chrome'))
+        self.results.append(self.options._import_options('headless_chrome'))
+        self.results.append(self.options._import_options('ie'))
+        self.results.append(self.options._import_options('opera'))
+        self.results.append(self.options._import_options('edge'))
+        self.results.append(self.error_formatter(self.options._import_options, 'phantomjs'))
+        self.results.append(self.error_formatter(self.options._import_options, 'safari'))
+        self.results.append(self.error_formatter(self.options._import_options, 'htmlunit'))
+        self.results.append(self.error_formatter(self.options._import_options, 'htmlunit_with_js'))
+        self.results.append(self.error_formatter(self.options._import_options, 'android'))
+        self.results.append(self.error_formatter(self.options._import_options, 'iphone'))
+        verify_all('Selenium options import', self.results, reporter=self.reporter)
+
     def result_formatter(self):
         if PY3:
             pass
         for index, result in enumerate(self.results):
             result = str(result)
             self.results[index] = result.replace("=u'", "='")
+
+    def error_formatter(self, method, arg):
+        try:
+            method(arg)
+        except Exception as error:
+            return '%s %s' % (arg, error.__str__()[:15])
