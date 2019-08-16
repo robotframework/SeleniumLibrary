269ec4ba920f5a613c887d0f6484c132983ddd0e
Tatu Aalto <aalto.tatu@gmail.com>
2019-08-17 02:58:40 +0300

    Register driver before EventFiringWebDriver is called

--- atest/acceptance/2-event_firing_webdriver/MyListener.py
+++ atest/acceptance/2-event_firing_webdriver/MyListener.py
@@ -1,4 +1,5 @@
 from robot.api import logger
+from robot.libraries.BuiltIn import BuiltIn
 from selenium.webdriver.support.events import AbstractEventListener
 
 
@@ -6,6 +7,9 @@ class MyListener(AbstractEventListener):
 
     def before_navigate_to(self, url, driver):
         logger.info("Before navigate to %s" % url)
+        sl = BuiltIn().get_library_instance('SeleniumLibrary')
+        if sl.driver:
+            logger.info('Got driver also from SeleniumLibrary.')
 
     def after_navigate_to(self, url, driver):
         logger.info("After navigate to %s" % url)
