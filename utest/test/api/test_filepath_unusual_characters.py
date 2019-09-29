d0cd01cd1f23082c84f7c79f82987a9f80ddc366
Tatu Aalto <aalto.tatu@gmail.com>
2019-09-30 00:03:46 +0300

    Add methot to replace Python format for PY2 and PY3

---
+++ utest/test/api/test_filepath_unusual_characters.py
@@ -0,0 +1,41 @@
+import os
+
+import pytest
+from robot.utils import JYTHON
+
+try:
+    from approvaltests.approvals import verify_all
+    from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
+except ImportError:
+    if JYTHON:
+        verify = None
+        GenericDiffReporterFactory = None
+    else:
+        raise
+
+from SeleniumLibrary.base import LibraryComponent
+
+
+@pytest.fixture(scope='module')
+def reporter():
+    if JYTHON:
+        return None
+    else:
+        path = os.path.dirname(__file__)
+        reporter_json = os.path.abspath(os.path.join(path, '..', 'approvals_reporters.json'))
+        factory = GenericDiffReporterFactory()
+        factory.load(reporter_json)
+        return factory.get_first_working()
+
+
+@pytest.mark.skipif(JYTHON, reason='ApprovalTest does not work with Jython')
+def test_normal_file_path(reporter):
+    results = []
+    lib = LibraryComponent(None)
+    results.append(lib.format_path('/foo/file.log', 1))
+    results.append(lib.format_path('/foo/file-{index}.log', 1))
+    results.append(lib.format_path('/foo/file-{index}.log', '0001'))
+    results.append(lib.format_path('/foo/file-{foo}.log', 1))
+    results.append(lib.format_path('/{foo}/file-{index}.log', 1))
+    results.append(lib.format_path('/foo/file-{index:03}.log', 1))
+    verify_all('Different file paths.', results, reporter=reporter)
