eefd17a689590cbb57d83c1b5899fc081d32c2a5
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2019-10-03 23:41:05 +0300

    Added more tests

--- utest/test/keywords/test_keyword_arguments_browsermanagement.py
+++ utest/test/keywords/test_keyword_arguments_browsermanagement.py
@@ -32,9 +32,28 @@ class KeywordArgumentsElementTest(unittest.TestCase):
                                           remote_url=remote_url)
         self.assertEqual(alias, None)
 
+    def test_same_alias(self):
+        url = 'https://github.com/robotframework'
+        alias = 'tidii'
+        driver = mock()
+        driver.session_id = 'foobar'
+        when(self.ctx._drivers).get_index(alias).thenReturn(1)
+        when(self.ctx._drivers).switch(1).thenReturn(driver)
+        self.brorser.open_browser(url=url, alias=alias)
+        verify(driver, times=1).get(url)
+
     def test_open_browser_no_get(self):
         browser = mock()
         when(self.brorser)._make_driver('firefox', None,
                                         None, False, None, None).thenReturn(browser)
         self.brorser.open_browser()
         verify(browser, times=0).get(ANY)
+
+    def test_same_alias_and_not_get(self):
+        alias = 'tidii'
+        driver = mock()
+        driver.session_id = 'foobar'
+        when(self.ctx._drivers).get_index(alias).thenReturn(1)
+        when(self.ctx._drivers).switch(1).thenReturn(driver)
+        self.brorser.open_browser(alias=alias)
+        verify(driver, times=0).get(url)
