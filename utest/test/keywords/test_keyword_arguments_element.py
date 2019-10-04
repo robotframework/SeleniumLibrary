98104b942a5a80d29d39e6ab2d472a951a6328b1
Tatu Aalto <aalto.tatu@gmail.com>
2019-10-05 00:06:03 +0300

    Fixing test_keyword_arguments_formelement.py to use pytest

--- utest/test/keywords/test_keyword_arguments_element.py
+++ utest/test/keywords/test_keyword_arguments_element.py
@@ -1,5 +1,3 @@
-import unittest
-
 import pytest
 from mockito import mock, unstub, when
 
--- utest/test/keywords/test_keyword_arguments_formelement.py
+++ utest/test/keywords/test_keyword_arguments_formelement.py
@@ -1,5 +1,4 @@
-import unittest
-
+import pytest
 from mockito import mock, unstub, when
 
 from SeleniumLibrary.keywords import FormElementKeywords
@@ -8,65 +7,80 @@ from SeleniumLibrary.keywords import FormElementKeywords
 FALSES = ['False', False, '', None, 'NONE']
 
 
-class KeywordArgumentsElementTest(unittest.TestCase):
+@pytest.fixture(scope='function')
+def form():
+    ctx = mock()
+    ctx.driver = mock()
+    return FormElementKeywords(ctx)
 
-    def setUp(self):
-        ctx = mock()
-        ctx._browser = mock()
-        self.form = FormElementKeywords(ctx)
-        self.ctx = ctx
 
-    def tearDown(self):
-        unstub()
+def teardown_function():
+    unstub()
 
-    def test_submit_form_false(self):
-        element = mock()
-        when(self.form).find_element('tag:form', tag='form').thenReturn(element)
-        for false in FALSES:
-            self.form.submit_form()
-        self.form.submit_form()
 
-    def test_submit_form_true(self):
-        element = mock()
-        when(self.form).find_element('//form', tag='form').thenReturn(element)
-        self.form.submit_form('//form')
+def test_submit_form_false(form):
+    element = mock()
+    when(form).find_element('tag:form', tag='form').thenReturn(element)
+    for false in FALSES:
+        form.submit_form()
+    form.submit_form()
 
-    def test_textfield_should_contain(self):
-        locator = '//input'
-        element = mock()
-        when(self.form).find_element(locator, 'text field').thenReturn(element)
-        when(element).get_attribute('value').thenReturn('no')
-        with self.assertRaisesRegexp(AssertionError, 'should have contained'):
-            self.form.textfield_should_contain(locator, 'text')
-        with self.assertRaisesRegexp(AssertionError, 'foobar'):
-            self.form.textfield_should_contain(locator, 'text', 'foobar')
+def test_submit_form_true(form):
+    element = mock()
+    when(form).find_element('//form', tag='form').thenReturn(element)
+    form.submit_form('//form')
 
-    def test_textfield_value_should_be(self):
-        locator = '//input'
-        element = mock()
-        when(self.form).find_element(locator, 'text field').thenReturn(element)
-        when(element).get_attribute('value').thenReturn('no')
-        with self.assertRaisesRegexp(AssertionError, 'text field'):
-            self.form.textfield_value_should_be(locator, 'value')
-        with self.assertRaisesRegexp(AssertionError, 'foobar'):
-            self.form.textfield_value_should_be(locator, 'value', 'foobar err')
+def test_textfield_should_contain(form):
+    locator = '//input'
+    element = mock()
+    when(form).find_element(locator, 'text field').thenReturn(element)
+    when(element).get_attribute('value').thenReturn('no')
+    with pytest.raises(AssertionError) as error:
+        form.textfield_should_contain(locator, 'text')
+    assert 'should have contained' in str(error.value)
 
-    def test_textarea_should_contain(self):
-        locator = '//input'
-        element = mock()
-        when(self.form).find_element(locator, 'text area').thenReturn(element)
-        when(element).get_attribute('value').thenReturn('no')
-        with self.assertRaisesRegexp(AssertionError, 'should have contained'):
-            self.form.textarea_should_contain(locator, 'value')
-        with self.assertRaisesRegexp(AssertionError, 'foobar error'):
-            self.form.textarea_should_contain(locator, 'value', 'foobar error')
+    with pytest.raises(AssertionError) as error:
+        form.textfield_should_contain(locator, 'text', 'foobar')
+    assert 'foobar' in str(error.value)
 
-    def test_textarea_value_should_be(self):
-        locator = '//input'
-        element = mock()
-        when(self.form).find_element(locator, 'text area').thenReturn(element)
-        when(element).get_attribute('value').thenReturn('no')
-        with self.assertRaisesRegexp(AssertionError, 'should have had'):
-            self.form.textarea_value_should_be(locator, 'value')
-        with self.assertRaisesRegexp(AssertionError, 'foobar'):
-            self.form.textarea_value_should_be(locator, 'value', 'foobar')
+
+def test_textfield_value_should_be(form):
+    locator = '//input'
+    element = mock()
+    when(form).find_element(locator, 'text field').thenReturn(element)
+    when(element).get_attribute('value').thenReturn('no')
+    with pytest.raises(AssertionError) as error:
+        form.textfield_value_should_be(locator, 'value')
+    assert 'text field' in str(error.value)
+
+    with pytest.raises(AssertionError) as error:
+        form.textfield_value_should_be(locator, 'value', 'foobar err')
+    assert 'foobar' in str(error.value)
+
+
+def test_textarea_should_contain(form):
+    locator = '//input'
+    element = mock()
+    when(form).find_element(locator, 'text area').thenReturn(element)
+    when(element).get_attribute('value').thenReturn('no')
+    with pytest.raises(AssertionError) as error:
+        form.textarea_should_contain(locator, 'value')
+    assert 'should have contained' in str(error.value)
+
+    with pytest.raises(AssertionError) as error:
+        form.textarea_should_contain(locator, 'value', 'foobar error')
+    assert 'foobar error' in str(error.value)
+
+
+def test_textarea_value_should_be(form):
+    locator = '//input'
+    element = mock()
+    when(form).find_element(locator, 'text area').thenReturn(element)
+    when(element).get_attribute('value').thenReturn('no')
+    with pytest.raises(AssertionError) as error:
+        form.textarea_value_should_be(locator, 'value')
+    assert 'should have had' in str(error.value)
+
+    with pytest.raises(AssertionError) as error:
+        form.textarea_value_should_be(locator, 'value', 'foobar')
+    assert 'foobar' in str(error.value)
