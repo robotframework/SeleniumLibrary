9c23ba0ea4136eb398e6e20b61ab7aa8976df8a6
Tatu Aalto <aalto.tatu@gmail.com>
2019-10-04 23:01:30 +0300

    Fixing test_keyword_arguments_element.py to use pytest

--- utest/test/keywords/test_keyword_arguments_element.py
+++ utest/test/keywords/test_keyword_arguments_element.py
@@ -1,35 +1,43 @@
 import unittest
 
+import pytest
 from mockito import mock, unstub, when
 
 from SeleniumLibrary.keywords import ElementKeywords
 
 
-class KeywordArgumentsElementTest(unittest.TestCase):
+@pytest.fixture(scope='function')
+def element():
+    ctx = mock()
+    ctx._browser = mock()
+    return ElementKeywords(ctx)
 
-    def setUp(self):
-        ctx = mock()
-        ctx._browser = mock()
-        self.element = ElementKeywords(ctx)
 
-    def tearDown(self):
-        unstub()
+def teardown_function():
+    unstub()
 
-    def test_locator_should_match_x_times(self):
-        locator = '//div'
-        when(self.element).find_elements(locator).thenReturn([])
-        with self.assertRaisesRegexp(AssertionError, 'should have matched'):
-            self.element.locator_should_match_x_times(locator, 1)
 
-        with self.assertRaisesRegexp(AssertionError, 'foobar'):
-            self.element.locator_should_match_x_times(locator, 1, 'foobar')
+def test_locator_should_match_x_times(element):
+    locator = '//div'
+    when(element).find_elements(locator).thenReturn([])
+    with pytest.raises(AssertionError) as error:
+        element.locator_should_match_x_times(locator, 1)
+    assert 'should have matched' in str(error.value)
 
-    def test_element_text_should_be(self):
-        locator = '//div'
-        element = mock()
-        element.text = 'text'
-        when(self.element).find_element(locator).thenReturn(element)
-        with self.assertRaisesRegexp(AssertionError, 'should have been'):
-            self.element.element_text_should_be(locator, 'not text')
-        with self.assertRaisesRegexp(AssertionError, 'foobar'):
-            self.element.element_text_should_be(locator, 'not text', 'foobar')
+    with pytest.raises(AssertionError) as error:
+        element.locator_should_match_x_times(locator, 1, 'foobar')
+    assert 'foobar' in str(error.value)
+
+
+def test_element_text_should_be(element):
+    locator = '//div'
+    webelement = mock()
+    webelement.text = 'text'
+    when(element).find_element(locator).thenReturn(webelement)
+    with pytest.raises(AssertionError) as error:
+        element.element_text_should_be(locator, 'not text')
+    assert 'should have been' in str(error.value)
+
+    with pytest.raises(AssertionError) as error:
+        element.element_text_should_be(locator, 'not text', 'foobar')
+    assert 'foobar' in str(error.value)
