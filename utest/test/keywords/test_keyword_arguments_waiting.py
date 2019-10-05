cdeee594a7f13511f51a1e14f25e88fb4a798919
Tatu Aalto <aalto.tatu@gmail.com>
2019-10-06 01:20:29 +0300

    Fixing test_keyword_arguments_waiting.py for pytest

--- utest/test/keywords/test_keyword_arguments_waiting.py
+++ utest/test/keywords/test_keyword_arguments_waiting.py
@@ -1,33 +1,42 @@
-import unittest
-
+import pytest
 from mockito import mock, unstub, when
 
 from SeleniumLibrary.keywords import WaitingKeywords
 
+TIMEOUT = 0.01
 
-class KeywordArgumentsWaitingKeywordsTest(unittest.TestCase):
 
-    def setUp(self):
-        self.ctx = mock()
-        self.ctx.driver = mock()
-        self.ctx.timeout = 0.01
-        self.waiting = WaitingKeywords(self.ctx)
+@pytest.fixture(scope='module')
+def waiting():
+    ctx = mock()
+    ctx.driver = mock()
+    ctx.timeout = TIMEOUT
+    return WaitingKeywords(ctx)
 
-    def tearDown(self):
-        unstub()
 
-    def test_wait_for_condition(self):
-        condition = 'return document.getElementById("intro")'
-        error = 'did not become true'
-        with self.assertRaisesRegexp(AssertionError, error):
-            self.waiting.wait_for_condition(condition)
-        with self.assertRaisesRegexp(AssertionError, 'foobar'):
-            self.waiting.wait_for_condition(condition, 'None', 'foobar')
+def teardown_module():
+    unstub()
 
-    def test_wait_until_page_contains(self):
-        text = 'text'
-        when(self.waiting).is_text_present(text).thenReturn(None)
-        with self.assertRaisesRegexp(AssertionError, "Text 'text' did not"):
-            self.waiting.wait_until_page_contains(text)
-        with self.assertRaisesRegexp(AssertionError, "error"):
-            self.waiting.wait_until_page_contains(text, 'None', 'error')
+
+def test_wait_for_condition(waiting):
+    condition = 'return document.getElementById("intro")'
+    error = 'did not become true'
+    with pytest.raises(AssertionError) as error:
+        waiting.wait_for_condition(condition)
+    assert 'did not become true' in str(error.value)
+
+    with pytest.raises(AssertionError) as error:
+        waiting.wait_for_condition(condition, 'None', 'foobar')
+    assert 'foobar' in str(error.value)
+
+
+def test_wait_until_page_contains(waiting):
+    text = 'text'
+    when(waiting).is_text_present(text).thenReturn(None)
+    with pytest.raises(AssertionError) as error:
+        waiting.wait_until_page_contains(text)
+    assert "Text 'text' did not" in str(error.value)
+
+    with pytest.raises(AssertionError) as error:
+        waiting.wait_until_page_contains(text, 'None', 'error')
+    assert 'error' in str(error.value)
