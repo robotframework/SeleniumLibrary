92b09d73b3f2f54772633cb9ab75df547456c842
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2019-12-12 22:28:03 +0200

    Fixing lib as Python 2 compatible

--- atest/resources/testlibs/custom_locator.py
+++ atest/resources/testlibs/custom_locator.py
@@ -6,5 +6,5 @@ from SeleniumLibrary import ElementFinder
 def custom_library_locator(parent, criteria, tag, constraints):
     sl = BuiltIn().get_library_instance('SeleniumLibrary')
     el = ElementFinder(sl)
-    elements = parent.find_elements_by_xpath(f'//{criteria}')
+    elements = parent.find_elements_by_xpath('//%s' % criteria)
     return el._filter_elements(elements, tag, constraints)
