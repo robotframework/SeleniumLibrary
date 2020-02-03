75bdea631a47c7b285810074a6bd3ba3d80bcb96
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2020-02-03 15:04:40 +0200

    Update version to 5.0.0.dev1

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -44,7 +44,7 @@ from SeleniumLibrary.locators import ElementFinder
 from SeleniumLibrary.utils import LibraryListener, timestr_to_secs, is_truthy
 
 
-__version__ = '4.2.1.dev1'
+__version__ = '5.0.0.dev1'
 
 
 class SeleniumLibrary(DynamicCore):
