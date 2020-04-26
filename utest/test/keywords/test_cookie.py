628dd616469733c124dbe11acfc828b6af1174dc
Tatu Aalto <aalto.tatu@gmail.com>
2020-04-27 00:06:14 +0300

    test_cookie.py to pytest

--- utest/test/keywords/test_cookie.py
+++ utest/test/keywords/test_cookie.py
@@ -1,114 +1,129 @@
 from datetime import datetime
-import unittest
 
-from mockito import mock, unstub, verify
+import pytest
+from mockito import mock, verify
 
 from SeleniumLibrary.keywords import CookieKeywords
 from SeleniumLibrary.keywords.cookie import CookieInformation
 
 
-class KeywordArgumentsCookieTest(unittest.TestCase):
-
-    def setUp(self):
-        self.ctx = mock()
-        self.ctx.driver = self.driver = mock()
-        self.cookie = CookieKeywords(self.ctx)
-        self.default_cookie = {'name': 'name', 'value': 'value'}
-
-    def tearDown(self):
-        unstub()
-
-    def test_add_cookie_default(self):
-        self.cookie.add_cookie('name', 'value')
-        verify(self.driver).add_cookie(self.default_cookie)
-
-    def test_add_cookie_secure_true(self):
-        cookie = self.default_cookie
-        cookie['secure'] = True
-        self.cookie.add_cookie('name', 'value', path='None', domain='None',
-                               secure='True')
-        verify(self.driver).add_cookie(cookie)
-        self.cookie.add_cookie('name', 'value', path='None', domain='None',
-                               secure='True_text')
-        verify(self.driver, times=2).add_cookie(cookie)
-        self.cookie.add_cookie('name', 'value', path='None', domain='None',
-                               secure=True)
-        verify(self.driver, times=3).add_cookie(cookie)
-        self.cookie.add_cookie('name', 'value', path='None', domain='None',
-                               secure='1')
-        verify(self.driver, times=4).add_cookie(cookie)
-
-    def test_add_cookie_secure_false(self):
-        self.cookie.add_cookie('name', 'value', path='None', domain='None',
-                               secure='None')
-        verify(self.driver).add_cookie(self.default_cookie)
-        self.cookie.add_cookie('name', 'value', path='None', domain='None',
-                               secure='False')
-        cookie = self.default_cookie
-        cookie['secure'] = False
-        verify(self.driver).add_cookie(cookie)
-        self.cookie.add_cookie('name', 'value', path='None', domain='None',
-                               secure=0)
-        verify(self.driver, times=2).add_cookie(cookie)
-
-    def test_add_cookie_domain_true(self):
-        self.cookie.add_cookie('name', 'value', path='None', domain='MyDomain',
-                               secure=None)
-        cookie = self.default_cookie
-        cookie['domain'] = 'MyDomain'
-        verify(self.driver).add_cookie(cookie)
-
-    def test_add_cookie_domain_false(self):
-        self.cookie.add_cookie('name', 'value', path='None', domain='None',
-                               secure=None)
-        verify(self.driver).add_cookie(self.default_cookie)
-
-    def test_add_cookie_path_true(self):
-        self.cookie.add_cookie('name', 'value', path='/foo/bar', domain=None,
-                               secure=None)
-        cookie = self.default_cookie
-        cookie['path'] = '/foo/bar'
-        verify(self.driver).add_cookie(cookie)
-
-    def test_add_cookie_path_false(self):
-        self.cookie.add_cookie('name', 'value', path='None', domain=None,
-                               secure=None)
-        verify(self.driver).add_cookie(self.default_cookie)
+ALL_ARGS = {'name': 'foo', 'value': '123', 'path': '/', 'domain': 'not.Here',
+            'secure': True, 'httpOnly': True, 'expiry': 123}
 
 
-class CookieObjecttest(unittest.TestCase):
+@pytest.fixture()
+def driver():
+    return mock()
 
-    all_args = {'name': 'foo', 'value': '123', 'path': '/', 'domain': 'not.Here',
-                'secure': True, 'httpOnly': True, 'expiry': 123}
 
-    def test_name_value_only(self):
-        cookie = CookieInformation(name='foo', value='bar')
-        self.assertEqual(cookie.name, 'foo')
-        self.assertEqual(cookie.value, 'bar')
+@pytest.fixture()
+def ctx(driver):
+    ctx = mock()
+    ctx.driver = driver
+    return ctx
 
-    def test_all_args(self):
-        cookie = CookieInformation(**self.all_args)
-        self.assertEqual(cookie.name, 'foo')
-        self.assertEqual(cookie.value, '123')
-        self.assertEqual(cookie.path, '/')
-        self.assertEqual(cookie.domain, 'not.Here')
-        self.assertEqual(cookie.secure, True)
-        self.assertEqual(cookie.httpOnly, True)
-        self.assertEqual(cookie.expiry, datetime.fromtimestamp(123))
-        self.assertEqual(cookie.extra, {})
+@pytest.fixture()
+def default_cookie():
+    return {'name': 'name', 'value': 'value'}
 
-    def test_extra_args(self):
-        cookie_dict = self.all_args.copy()
-        cookie_dict['class_name'] = 'seleniumLibary'
-        cookie = CookieInformation(**cookie_dict)
-        self.assertEqual(cookie.name, 'foo')
-        self.assertEqual(cookie.value, '123')
-        self.assertEqual(cookie.extra, {'class_name': 'seleniumLibary'})
-        string = str(cookie)
-        self.assertIn("\nextra={'class_name': 'seleniumLibary'}", string)
 
-    def test_no_mandatory_args(self):
-        cookie_dict = self.all_args.copy()
-        del cookie_dict['name']
-        with self.assertRaises(TypeError):
-            CookieInformation(**cookie_dict)
+@pytest.fixture()
+def cookie(ctx):
+    return CookieKeywords(ctx)
+
+
+def test_add_cookie_default(cookie, driver, default_cookie):
+    cookie.add_cookie('name', 'value')
+    verify(driver).add_cookie(default_cookie)
+
+
+def test_add_cookie_secure_true(cookie, default_cookie, driver):
+    default_cookie['secure'] = True
+    cookie.add_cookie('name', 'value', path='None', domain='None',
+                      secure='True')
+    verify(driver).add_cookie(default_cookie)
+    cookie.add_cookie('name', 'value', path='None', domain='None',
+                      secure='True_text')
+    verify(driver, times=2).add_cookie(default_cookie)
+    cookie.add_cookie('name', 'value', path='None', domain='None',
+                      secure=True)
+    verify(driver, times=3).add_cookie(default_cookie)
+    cookie.add_cookie('name', 'value', path='None', domain='None',
+                      secure='1')
+    verify(driver, times=4).add_cookie(default_cookie)
+
+
+def test_add_cookie_secure_false(cookie, driver, default_cookie):
+    cookie.add_cookie('name', 'value', path='None', domain='None',
+                      secure='None')
+    verify(driver).add_cookie(default_cookie)
+    cookie.add_cookie('name', 'value', path='None', domain='None',
+                      secure='False')
+    default_cookie['secure'] = False
+    verify(driver).add_cookie(default_cookie)
+    cookie.add_cookie('name', 'value', path='None', domain='None',
+                      secure=0)
+    verify(driver, times=2).add_cookie(default_cookie)
+
+
+def test_add_cookie_domain_true(cookie, default_cookie, driver):
+    cookie.add_cookie('name', 'value', path='None', domain='MyDomain',
+                      secure=None)
+    cookie = default_cookie
+    cookie['domain'] = 'MyDomain'
+    verify(driver).add_cookie(cookie)
+
+
+def test_add_cookie_domain_false(cookie, driver, default_cookie):
+    cookie.add_cookie('name', 'value', path='None', domain='None',
+                      secure=None)
+    verify(driver).add_cookie(default_cookie)
+
+
+def test_add_cookie_path_true(cookie, default_cookie, driver):
+    cookie.add_cookie('name', 'value', path='/foo/bar', domain=None,
+                      secure=None)
+    default_cookie['path'] = '/foo/bar'
+    verify(driver).add_cookie(default_cookie)
+
+
+def test_add_cookie_path_false(cookie, driver, default_cookie):
+    cookie.add_cookie('name', 'value', path='None', domain=None,
+                      secure=None)
+    verify(driver).add_cookie(default_cookie)
+
+
+def test_name_value_only():
+    cookie = CookieInformation(name='foo', value='bar')
+    assert cookie.name == 'foo'
+    assert cookie.value == 'bar'
+
+
+def test_all_args():
+    cookie = CookieInformation(**ALL_ARGS)
+    assert cookie.name == 'foo'
+    assert cookie.value == '123'
+    assert cookie.path == '/'
+    assert cookie.domain == 'not.Here'
+    assert cookie.secure == True
+    assert cookie.httpOnly == True
+    assert cookie.expiry == datetime.fromtimestamp(123)
+    assert cookie.extra == {}
+
+
+def test_extra_args():
+    cookie_dict = ALL_ARGS.copy()
+    cookie_dict['class_name'] = 'seleniumLibary'
+    cookie = CookieInformation(**cookie_dict)
+    assert cookie.name == 'foo'
+    assert cookie.value == '123'
+    assert cookie.extra  == {'class_name': 'seleniumLibary'}
+    string = str(cookie)
+    assert "\nextra={'class_name': 'seleniumLibary'}" in string
+
+
+def test_no_mandatory_args():
+    cookie_dict = ALL_ARGS.copy()
+    del cookie_dict['name']
+    with pytest.raises(TypeError):
+        CookieInformation(**cookie_dict)
