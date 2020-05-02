ed4be28b006d1d478dd598160ed296024c0f5841
Tatu Aalto <aalto.tatu@gmail.com>
2020-05-02 20:29:31 +0300

    Take pytest-approvaltests in use

--- utest/run.py
+++ utest/run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import argparse
 import os
 import shutil
 import sys
@@ -19,15 +19,26 @@ def remove_output_dir():
     os.mkdir(output_dir)
 
 
-def run_unit_tests():
+def run_unit_tests(reporter, reporter_args):
     sys.path.insert(0, SRC)
+    py_args = ['--rootdir=%s' % CURDIR, '-p', 'no:cacheprovider', CURDIR]
+    if reporter:
+        py_args.insert(0, f'--approvaltests-add-reporter={reporter}')
+    if reporter_args:
+        py_args.insert(1, f'--approvaltests-add-reporter-args={reporter_args}')
     try:
-        result = py_main(['--rootdir=%s' % CURDIR, '-p', 'no:cacheprovider', CURDIR])
+        result = py_main(py_args)
+    except Exception:
+        result = 254
     finally:
         sys.path.pop(0)
     return result
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='SeleniumLibrary Unit test runner.')
+    parser.add_argument('-R', '--approvaltests-use-reporter', default='PythonNative', dest='reporter')
+    parser.add_argument('-A', '--approvaltests-add-reporter-args', default=None, dest='reporter_args')
+    args = parser.parse_args()
     remove_output_dir()
-    sys.exit(run_unit_tests())
+    sys.exit(run_unit_tests(args.reporter, args.reporter_args))
