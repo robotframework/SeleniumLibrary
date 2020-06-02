db0de9d4fc2f20a8041169a32d7518276d021d4e
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2020-06-02 13:11:58 +0300

    Fix doc bug in page_should_not_contain_image

--- src/SeleniumLibrary/keywords/element.py
+++ src/SeleniumLibrary/keywords/element.py
@@ -1002,7 +1002,7 @@ return !element.dispatchEvent(evt);
 
     @keyword
     def page_should_not_contain_image(self, locator, message=None, loglevel='TRACE'):
-        """Verifies image identified by ``locator`` is found from current page.
+        """Verifies image identified by ``locator`` is not found from current page.
 
         See the `Locating elements` section for details about the locator
         syntax. When using the default locator strategy, images are searched
