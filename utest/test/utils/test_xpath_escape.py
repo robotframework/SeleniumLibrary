59a68ace6dd4d1b8dfe495c7e4f52f0773c130f3
Tatu Aalto <aalto.tatu@gmail.com>
2020-09-10 23:32:42 +0300

    Test escape_xpath_value

---
+++ utest/test/utils/test_xpath_escape.py
@@ -0,0 +1,35 @@
+import os
+
+import pytest
+from approvaltests.approvals import verify_all
+from approvaltests.reporters.generic_diff_reporter_factory import (
+    GenericDiffReporterFactory,
+)
+from robot.utils import WINDOWS
+
+from SeleniumLibrary.utils import escape_xpath_value
+
+
+@pytest.fixture(scope="module")
+def reporter():
+    path = os.path.dirname(__file__)
+    reporter_json = os.path.abspath(
+        os.path.join(path, "..", "approvals_reporters.json")
+    )
+    factory = GenericDiffReporterFactory()
+    factory.load(reporter_json)
+    return factory.get_first_working()
+
+
+@pytest.mark.skipif(WINDOWS, reason="ApprovalTest do not support different line feeds")
+def test_string(reporter):
+    results = []
+    results.append(escape_xpath_value("tidii"))
+    results.append(escape_xpath_value('"tidii"'))
+    results.append(escape_xpath_value("'tidii'"))
+    results.append(escape_xpath_value("\"'tidii'"))
+    results.append(escape_xpath_value("\"'tidii'\""))
+    results.append(escape_xpath_value("\"'t\"id\"ii'\""))
+    results.append(escape_xpath_value('"\'tidii\'"'))
+    results.append(escape_xpath_value('"\'ti\'d\'ii\'"'))
+    verify_all("Escape xpath value", results, reporter=reporter)
