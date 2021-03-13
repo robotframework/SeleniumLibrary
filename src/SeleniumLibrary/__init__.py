4835358691045555959372daa78056eb21404a20
Tatu Aalto <aalto.tatu@gmail.com>
2021-03-14 00:29:48 +0200

    Back to dev version

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -51,7 +51,7 @@ from SeleniumLibrary.locators import ElementFinder
 from SeleniumLibrary.utils import LibraryListener, is_truthy, _convert_timeout
 
 
-__version__ = "5.1.1"
+__version__ = "5.2.0.dev1"
 
 
 class SeleniumLibrary(DynamicCore):
