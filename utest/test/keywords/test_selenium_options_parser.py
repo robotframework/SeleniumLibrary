1cb2c3edec9aaecff64883c4258479a5c67ebeb5
Ed Manlove <emanlove@verizon.net>
2021-12-11 07:49:06 -0500

    Updated unit tests related to Safari
    - Safari WebDriver now has options

--- utest/test/keywords/test_selenium_options_parser.py
+++ utest/test/keywords/test_selenium_options_parser.py
@@ -350,7 +350,7 @@ def test_has_options(creator):
     assert creator._has_options(webdriver.Ie)
     assert creator._has_options(webdriver.Edge)
     assert creator._has_options(webdriver.Opera)
-    assert creator._has_options(webdriver.Safari) is False
+    assert creator._has_options(webdriver.Safari)
 
 
 def test_create_opera_with_options(creator):
