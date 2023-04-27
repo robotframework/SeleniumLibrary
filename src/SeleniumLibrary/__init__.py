65027e603893fc9b2e15bf25d08368bece2d5484
Ed Manlove <emanlove@verizon.net>
2023-04-27 15:40:51 -0400

    Moved action_chain_delay argument to last position for backwards compatability
    As with the addition of the page_load_timeout, moved action_chain_delay
    into the last position so if anyone has poorly structured Library
    imports (which should be using named arguments) this will not break
    their implementations.

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -442,12 +442,12 @@ class SeleniumLibrary(DynamicCore):
         self,
         timeout=timedelta(seconds=5),
         implicit_wait=timedelta(seconds=0),
-        action_chain_delay=timedelta(seconds=0.25),
         run_on_failure="Capture Page Screenshot",
         screenshot_root_directory: Optional[str] = None,
         plugins: Optional[str] = None,
         event_firing_webdriver: Optional[str] = None,
         page_load_timeout=timedelta(minutes=5),
+        action_chain_delay=timedelta(seconds=0.25),
     ):
         """SeleniumLibrary can be imported with several optional arguments.
 
@@ -455,8 +455,6 @@ class SeleniumLibrary(DynamicCore):
           Default value for `timeouts` used with ``Wait ...`` keywords.
         - ``implicit_wait``:
           Default value for `implicit wait` used when locating elements.
-        - ``action_chain_delay``:
-          Default value for `ActionChains` delay to wait in between actions.
         - ``run_on_failure``:
           Default action for the `run-on-failure functionality`.
         - ``screenshot_root_directory``:
@@ -470,6 +468,8 @@ class SeleniumLibrary(DynamicCore):
           [https://seleniumhq.github.io/selenium/docs/api/py/webdriver_support/selenium.webdriver.support.event_firing_webdriver.html#module-selenium.webdriver.support.event_firing_webdriver|EventFiringWebDriver]
         - ``page_load_timeout``:
           Default value to wait for page load to complete until error is raised.
+        - ``action_chain_delay``:
+          Default value for `ActionChains` delay to wait in between actions.
         """
         self.timeout = _convert_timeout(timeout)
         self.implicit_wait = _convert_timeout(implicit_wait)
