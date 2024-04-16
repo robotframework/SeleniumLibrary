8f2839df0c66c46dd7a3c30162620d12eeeee2d3
Ed Manlove <emanlove@verizon.net>
2024-04-16 08:18:32 -0400

    Updated version to 6.3.0rc2

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -52,7 +52,7 @@ from SeleniumLibrary.locators import ElementFinder
 from SeleniumLibrary.utils import LibraryListener, is_truthy, _convert_timeout, _convert_delay
 
 
-__version__ = "6.3.0rc1"
+__version__ = "6.3.0rc2"
 
 
 class SeleniumLibrary(DynamicCore):
