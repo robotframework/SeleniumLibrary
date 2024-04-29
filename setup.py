39bd3f6b5a987718c0ea0ec2d5c78766ead3ff40
Ed Manlove <emanlove@verizon.net>
2024-04-28 22:09:35 -0400

    Updated required and supported Python versions within setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,10 @@ License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.6
-Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
 Programming Language :: Python :: 3 :: Only
 Topic :: Software Development :: Testing
 Framework :: Robot Framework
@@ -40,7 +41,7 @@ setup(
     keywords         = 'robotframework testing testautomation selenium webdriver web',
     platforms        = 'any',
     classifiers      = CLASSIFIERS,
-    python_requires  = '>=3.6, <4',
+    python_requires  = '>=3.8, <3.12',
     install_requires = REQUIREMENTS,
     package_dir      = {'': 'src'},
     packages         = find_packages('src'),
