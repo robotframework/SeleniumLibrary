a520e8af118e16fad9f5dce57145e76b34211941
Tatu Aalto <aalto.tatu@gmail.com>
2024-05-18 22:47:15 +0300

    Fix tests

--- utest/test/translation/test_translation.py
+++ utest/test/translation/test_translation.py
@@ -8,11 +8,17 @@ from SeleniumLibrary import SeleniumLibrary
 
 @pytest.fixture()
 def sl() -> SeleniumLibrary:
-    sys.path.append(str(Path(__file__).parent.absolute()))
+    d = Path(__file__).parent.parent.absolute()
+    sys.path.append(str(Path(__file__).parent.parent.absolute()))
     return SeleniumLibrary(language="FI")
+
 
 def test_translation(sl: SeleniumLibrary):
     spec = sl.keywords_spec["__init__"]
     assert spec.argument_specification
     doc: str = spec.documentation
     assert doc.startswith("1 SeleniumLibrary is a web testing library for Robot Framework")
+
+    spec = sl.keywords_spec["hallinnoi_hälytys"]
+    doc: str = spec.documentation
+    assert doc == "Hallinnoi hälytyksen uusi dokkari\n\nToinen rivi"
