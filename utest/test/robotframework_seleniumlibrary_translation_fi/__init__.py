6a0665e561ce9100d1ab0b64077e4c5409e997a4
Tatu Aalto <aalto.tatu@gmail.com>
2024-05-18 22:47:15 +0300

    Fix tests

---
+++ utest/test/robotframework_seleniumlibrary_translation_fi/__init__.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def get_language() -> dict:
+    curr_dir = Path(__file__).parent.absolute()
+    return {
+        "language": "fi",
+        "path": curr_dir / "translate.json"
+    }
