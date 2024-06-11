eefb1ca5e37a93bf0f8fba625c4e06765325a634
Ed Manlove <emanlove@verizon.net>
2024-06-10 20:53:01 -0400

    Updated version to 6.5.0rc1

--- src/SeleniumLibrary/__init__.py
+++ src/SeleniumLibrary/__init__.py
@@ -55,7 +55,7 @@ from SeleniumLibrary.locators import ElementFinder
 from SeleniumLibrary.utils import LibraryListener, is_truthy, _convert_timeout, _convert_delay
 
 
-__version__ = "6.4.0"
+__version__ = "6.5.0rc1"
 
 
 class SeleniumLibrary(DynamicCore):
