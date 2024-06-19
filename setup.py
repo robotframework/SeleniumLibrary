688bcc8a94a36e46437f4b147b53c94014473bbf
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2024-06-19 22:40:22 +0300

    Update setup.py to Python 3.12 support

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
 Programming Language :: Python :: 3 :: Only
 Topic :: Software Development :: Testing
 Framework :: Robot Framework
