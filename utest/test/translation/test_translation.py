b5986f49e0906b8e3cab58dc0ba35211df4a9f55
Tatu Aalto <aalto.tatu@gmail.com>
2024-09-08 12:37:22 +0300

    Enhance transtion to discover multiple translation from same Python plugin
    Fixes #1917

--- utest/test/translation/test_translation.py
+++ utest/test/translation/test_translation.py
@@ -17,9 +17,20 @@ def test_translation(sl: SeleniumLibrary):
     assert spec.argument_specification
     doc: str = spec.documentation
     assert doc.startswith(
-        "1 SeleniumLibrary is a web testing library for Robot Framework"
+        "00 SeleniumLibrary is a web testing library for Robot Framework"
     )
 
     spec = sl.keywords_spec["hallinnoi_hälytys"]
     doc: str = spec.documentation
     assert doc == "Hallinnoi hälytyksen uusi dokkari\n\nToinen rivi"
+
+
+def test_provide_translation_as_list(sl: SeleniumLibrary):
+    lang_plugin = "robotframework_seleniumlibrary_translation_list"
+    file_path = Path(__file__).parent.parent / lang_plugin / "translate2.json"
+    received_path = sl._get_translation("swe")
+    assert received_path == file_path, received_path.relative_to(file_path)
+    assert sl._get_translation("wrong") is None
+    received_path = sl._get_translation("Eng")
+    file_path = Path(__file__).parent.parent / lang_plugin / "translate1.json"
+    assert received_path == file_path, received_path.relative_to(file_path)
